02c3143f6101afd5482f43e22cd2f11d141c28b0
Dan Lovell <dlovell@gmail.com>
2012-04-05 15:14:38 -0400

    debugging DPMB_basic.py

--- examples/DPMB_basic.py
+++ examples/DPMB_basic.py
@@ -17,25 +17,29 @@ from IPython.parallel import *
 # TODO: Clean up naming of load balanced vs direct views
 Cloudless.base.remote_mode()
 Cloudless.base.remote_exec('import Cloudless.examples.DPMB as dm')
+Cloudless.base.remote_exec('import time')
+Cloudless.base.remote_exec('import numpy')
 import Cloudless.examples.DPMB as dm
+import time
+import numpy
 
 # block 4
 # definition of the job (re-eval to change code)
-def sampler(gen_seed,inf_seed,rows,cols,alpha,beta):
+def sampler(gen_seed,inf_seed,rows,cols,alpha,beta,num_iters):
     dataset = dm.gen_dataset(gen_seed,rows,cols,alpha,beta)
-    sample = gen_sample(inf_seed, dataset, num_iters,None,None)
+    sample = dm.gen_sample(inf_seed, dataset, num_iters,None,None)
     return sample
 
 #TODO: make this a decorator; think carefully about dependencies, reloading
 Cloudless.base.remote_procedure('sampler', sampler)
 
 # block 5
-def raw_testjob(gen_seed,inf_seed,rows,cols,alpha,beta):
+def raw_testjob(gen_seed,inf_seed,rows,cols,alpha,beta,num_iters):
     time.sleep(numpy.random.uniform(1))
-    return sampler(gen_seed,inf_seed,rows,cols,alpha,beta)
+    return sampler(gen_seed,inf_seed,rows,cols,alpha,beta,num_iters)
 
 # make memoized job (re-eval if the job code changes, or to reset cache)
-testjob = Cloudless.memo.AsyncMemoize("testjob", ["gen_seed","inf_seed","rows","cols","alpha","beta"], raw_testjob, override=True)
+testjob = Cloudless.memo.AsyncMemoize("testjob", ["gen_seed","inf_seed","rows","cols","alpha","beta","num_iters"], raw_testjob, override=True)
 
 # block 6
 # set constants (re-eval to change the scope of the plot)
@@ -43,7 +47,7 @@ XRANGE = 4
 
 # request the computation (re-eval if e.g. the range changes)
 for x in range(XRANGE):
-    testjob(0,0,10,10*(x+1),1,1)
+    testjob(0,0,10,10*(x+1),1,1,3)
 
 # block 7
 # get plot data locally (re-eval to get more data)
@@ -52,7 +56,7 @@ xs = []
 ys = []
 for (k, v) in testjob.iter():
     xs.append(k[0])
-    deltaT = sum([x["timing"]["zs"]["stop"]-x["timing"]["zs"]["start"] for x in v])
+    deltaT = sum([x["timing"]["zs"]["stop"]-x["timing"]["zs"]["start"] for x in v["stats"]])
     ys.append(deltaT)
 
 # block 8
