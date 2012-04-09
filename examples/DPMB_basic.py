26cac28a8ace23d5fd880b68c8a66f55814e2752
Dan Lovell <dlovell@gmail.com>
2012-04-09 15:23:01 -0400

    fixed typo bug, made inference problem larger to test predictive_prob dynamics, pulling out more variables for plotting

--- examples/DPMB_basic.py
+++ examples/DPMB_basic.py
@@ -28,7 +28,7 @@ import numpy as np
 def raw_testjob(gen_seed,inf_seed,rows,cols,alpha,beta,num_iters):
     gen_state_with_data = dm.gen_dataset(gen_seed,rows,cols,alpha,beta)
     gen_sample_output = dm.gen_sample(inf_seed, gen_state_with_data["observables"], num_iters,None,None,num_train=int(np.floor(.9*rows)))
-    predictive_prob = test_model(gen_state_with_data,gen_sample_output["state"],num_train=int(np.floor(.9*rows)))
+    predictive_prob = dm.test_model(gen_state_with_data,gen_sample_output["state"],num_train=int(np.floor(.9*rows)))
     return gen_sample_output,predictive_prob
 # make memoized job (re-eval if the job code changes, or to reset cache)
 testjob = Cloudless.memo.AsyncMemoize("testjob", ["gen_seed","inf_seed","rows","cols","alpha","beta","num_iters"], raw_testjob, override=True)
@@ -36,32 +36,40 @@ testjob = Cloudless.memo.AsyncMemoize("testjob", ["gen_seed","inf_seed","rows","
 
 # block 4
 # set constants (re-eval to change the scope of the plot)
-XRANGE = 10
+XRANGE = 4
 # request the computation (re-eval if e.g. the range changes)
 for x in range(XRANGE):
-    testjob(0,x,1000,20,1,1,10)
+    testjob(0,x,4000,40,1,1,6)
 
 
 # block 5
 # get plot data locally (re-eval to get more data)
 status = testjob.report_status()
-xs = []
-ys = []
+time_delta = []
+log_score = []
+predictive_prob = []
 for (k, v) in testjob.iter():
-    xs.append(np.array([x["timing"]["zs"]["delta"].total_seconds() for x in v["stats"]]).cumsum())
-    ys.append(np.array([x["score"] for x in v["stats"]]))
+    time_delta.append(np.array([x["timing"]["zs"]["delta"].total_seconds() for x in v[0]["stats"]]).cumsum())
+    log_score.append(np.array([x["score"] for x in v[0]["stats"]]))
+    predictive_prob.append(np.array([x["predictive_prob"]["sampled_prob"] for x in v[0]["stats"]]))    
+
 
 # block 6
 # make a plot (iterate on this block to fix layout/display issues)
 pylab.figure()
-pylab.plot(np.array(xs).T, np.array(ys).T)
+pylab.plot(np.array(time_delta).T, np.array(predictive_prob).T)
 pylab.xlabel('Time Elapsed')
-pylab.ylabel('log score')
+pylab.ylabel('predictive_prob')
 pylab.show()
 pylab.savefig('basic-job-results.png')
 ##
-print sample["stats"][0]["predictive_prob"]["gen_prob"]
-print [stats["predictive_prob"]["sampled_prob"] for stats in sample["stats"]]
+pylab.figure()
+pylab.plot(np.array(time_delta).T, np.array(log_score).T)
+pylab.xlabel('Time Elapsed')
+pylab.ylabel('log_score')
+pylab.show()
+pylab.savefig('basic-job-results.png')
+
 
 # block 7
 # examine the exceptions for the jobs that failed
