1a0f39f425661320fdb4c51e3c69e33bd4ba0080
Dan Lovell <dlovell@gmail.com>
2012-04-23 00:38:41 -0400

    png, not jpg

--- examples/plot_test_states.py
+++ examples/plot_test_states.py
@@ -103,8 +103,8 @@ for run_spec in ALL_RUN_SPECS:
 
 # now you can interactively call
 target_problem = ALL_PROBLEMS[0]
-hf.plot_measurement(memoized_infer, "num_clusters", target_problem,save_str="num_clusters.jpg")
-hf.plot_measurement(memoized_infer, ("ari", target_problem["zs"]), target_problem,save_str="ari.jpg")
+hf.plot_measurement(memoized_infer, "num_clusters", target_problem,save_str="num_clusters.png")
+hf.plot_measurement(memoized_infer, ("ari", target_problem["zs"]), target_problem,save_str="ari.png")
 #hf.plot_measurement(memoized_infer, "predictive", target_problem)
 
 # with open("pickled_jobs.pkl","wb") as fh:
