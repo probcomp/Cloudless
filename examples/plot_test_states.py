9a834a2b35ad14ecb68e515cd2a79664a13f382a
Dan Lovell <dlovell@gmail.com>
2012-04-24 22:28:34 -0400

    made plot_test_states_big.py actually big again and moved to plot_test_states.py

--- examples/plot_test_states.py
+++ examples/plot_test_states.py
@@ -1,4 +1,4 @@
-import matplotlib
+import cPickle,matplotlib,sys
 matplotlib.use('Agg')
 import DPMB_plotutils as dp
 reload(dp)
@@ -9,7 +9,9 @@ reload(hf)
 import numpy as np
 import matplotlib.pylab as pylab
 ##
-import sys
+if sys.platform == "win32":
+    sys.path.append("c:/")
+    
 import Cloudless
 reload(Cloudless)
 import Cloudless.memo
@@ -19,15 +21,17 @@ reload(Cloudless.memo)
 # block 2
 # configure remote nodes
 # TODO: Clean up naming of load balanced vs direct views
-Cloudless.base.remote_mode()
-Cloudless.base.remote_exec('import Cloudless.examples.DPMB_plotutils as dp')
-Cloudless.base.remote_exec('reload(dp)')
-Cloudless.base.remote_exec('import Cloudless.examples.DPMB_State as ds')
-Cloudless.base.remote_exec('reload(ds)')
-Cloudless.base.remote_exec('import Cloudless.examples.DPMB_helper_functions as hf')
-Cloudless.base.remote_exec('reload(hf)')
-Cloudless.base.remote_exec('import numpy as np')
-Cloudless.base.remote_exec('import matplotlib.pylab as pylab')
+if sys.platform != "win32":
+    Cloudless.base.remote_mode()
+    Cloudless.base.remote_exec('import Cloudless.examples.DPMB_plotutils as dp')
+    Cloudless.base.remote_exec('reload(dp)')
+    Cloudless.base.remote_exec('import Cloudless.examples.DPMB_State as ds')
+    Cloudless.base.remote_exec('reload(ds)')
+    Cloudless.base.remote_exec('import Cloudless.examples.DPMB_helper_functions as hf')
+    Cloudless.base.remote_exec('reload(hf)')
+    Cloudless.base.remote_exec('import numpy as np')
+    Cloudless.base.remote_exec('import matplotlib.pylab as pylab')
+    
 import Cloudless.examples.DPMB_plotutils as dp
 reload(dp)
 import Cloudless.examples.DPMB_State as ds
@@ -40,7 +44,7 @@ import matplotlib.pylab as pylab
 
 ALL_DATASET_SPECS = []
 
-for num_clusters in [2,8,32]:##[2**(j+1) for j in [2]]:
+for num_clusters in [2,8,32,]:##[2**(j+1) for j in [2]]:
     dataset_spec = {}
     dataset_spec["gen_seed"] = 0
     dataset_spec["num_cols"] = 64
@@ -67,7 +71,7 @@ print "Generated " + str(len(ALL_PROBLEMS)) + " problems!"
 # NOTE: Can clean up using itertools.product()
 # http://docs.python.org/library/itertools.html#itertools.product
 ALL_RUN_SPECS = []
-num_iters = 10
+num_iters = 30
 count = 0
 for problem in ALL_PROBLEMS:
     for infer_seed in range(1):
@@ -80,12 +84,15 @@ for problem in ALL_PROBLEMS:
                             run_spec["num_iters"] = num_iters
                             run_spec["infer_seed"] = infer_seed
                             run_spec["infer_init_alpha"] = infer_init_alpha
-                            run_spec["infer_init_betas"] = infer_init_betas
+                            run_spec["infer_init_betas"] = infer_init_betas.copy()
                             run_spec["infer_do_alpha_inference"] = infer_do_alpha_inference
                             run_spec["infer_do_betas_inference"] = infer_do_betas_inference
                             run_spec["infer_init_z"] = infer_init_z
                             run_spec["problem"] = problem
-                            ALL_RUN_SPECS.append(run_spec)
+                            ##
+                            run_spec["time_seatbelt"] = 5.0
+                            run_spec["ari_seatbelt"] = .9
+                            ALL_RUN_SPECS.append(run_spec) ## this seems to make the comparison fail copy.deepcopy(run_spec))
 
 print "Generated " + str(len(ALL_RUN_SPECS)) + " run specs!"
 # until success:
@@ -101,14 +108,27 @@ print "Created memoizer"
 for run_spec in ALL_RUN_SPECS:
     memoized_infer(run_spec)
 
+run_spec_filter = None ## lambda x: x["infer_init_z"] is None ## 
+
 # now you can interactively call
 for problem_idx,target_problem in enumerate(ALL_PROBLEMS):
-    config_str = ("cols" + str(target_problem["dataset_spec"]["num_cols"]) + "_rows"
-                  + str(target_problem["dataset_spec"]["num_rows"]) + "_clusters" + str(target_problem["dataset_spec"]["gen_z"][1]))
-    hf.plot_measurement(memoized_infer, "num_clusters", target_problem,save_str="num_clusters_" + config_str + ".png",title_str="num_clusters")
-    hf.plot_measurement(memoized_infer, ("ari", target_problem["zs"]), target_problem,save_str="ari_" + config_str + ".png",title_str="ari")
-    
+    # FIXME : verify that the new gen_z works and the cluster_str below is correct
+    cluster_str = "_clusters" + str(target_problem["dataset_spec"]["gen_z"][1]) ##
+    col_str = "cols" + str(target_problem["dataset_spec"]["num_cols"])
+    row_str = "rows" + str(target_problem["dataset_spec"]["num_rows"])
+    cluster_str = "clusters" + str(cluster_str) ## 
+    config_str = "_".join([col_str,row_str,cluster_str])    
+    # hf.plot_measurement(memoized_infer, "num_clusters", target_problem,save_str="num_clusters_" + config_str + ".png"
+    #                     ,title_str="num_clusters",ylabel_str="num_clusters")
+    try:
+        hf.plot_measurement(memoized_infer, ("ari", target_problem["zs"]), target_problem, run_spec_filter=run_spec_filter
+                            ,save_str="ari_" + config_str + ".png",title_str=[config_str,"ari"],ylabel_str="ari"
+                            ,legend_args={"ncol":2,"markerscale":2})
+    except Exception, e:
+        print e
+        
 #hf.plot_measurement(memoized_infer, "predictive", target_problem)
 
-# with open("pickled_jobs.pkl","wb") as fh:
-#     cPickle.dump({"memo":memoized_infer.memo,"ALL_RUN_SPECS":ALL_RUN_SPECS},fh)
+if memoized_infer.report_status["waiting"] == 0:
+    with open("pickled_jobs.pkl","wb") as fh:
+        cPickle.dump(memoized_infer.memo,fh)
