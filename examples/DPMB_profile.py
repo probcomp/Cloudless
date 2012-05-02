d387143e03db58c5b7c6a8e65a397548a806c1d1
dan lovell <dlovell@gmail.com>
2012-05-02 16:14:58 -0400

    reorged _profile

--- examples/DPMB_profile.py
+++ examples/DPMB_profile.py
@@ -3,9 +3,12 @@ import pstats
 ##http://docs.python.org/library/profile.html#module-cProfile
 
 script_str = "DPMB_Diagnostics/plot_test_states_local.py"
-output_file = "profile.out"
-cmd_str = "python -m cProfile -o " + output_file + " " + script_str 
-os.system(cmd_str)
-##
-p = pstats.Stats(output_file)
+prof_file = os.path.splitext(script_str)[0] + ".prof"
+
+if not os.path.isfile(prof_file):
+    cmd_str = ("python -m cProfile -o " + prof_file 
+               + " " + script_str )
+    sys_out = os.system(cmd_str)
+
+p = pstats.Stats(prof_file)
 p.sort_stats('cumulative').print_stats(10)
