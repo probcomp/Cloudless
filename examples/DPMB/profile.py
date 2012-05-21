df9be6bd36180d0ca5479858cea66f842b68ca70
dan lovell <dlovell@gmail.com>
2012-05-21 09:40:09 -0400

    make profile.py take the script to profile as an argument, correct the sort_stats argument

--- examples/DPMB/profile.py
+++ examples/DPMB/profile.py
@@ -2,15 +2,16 @@ import os,sys
 import pstats
 ##http://docs.python.org/library/profile.html#module-cProfile
 
-num_lines = 40 if len(sys.argv) < 2 else int(sys.argv[1])
+if len(sys.argv) < 2:
+    raise Exception("must pass a script argument")
 
-script_str = "DPMB_Diagnostics/plot_test_states_local.py"
+script_str = sys.argv[1]
+num_lines = 40 if len(sys.argv) < 3 else int(sys.argv[2])
+
 prof_file = os.path.splitext(script_str)[0] + ".prof"
-
 if not os.path.isfile(prof_file):
     cmd_str = ("python -m cProfile -o " + prof_file 
                + " " + script_str )
     sys_out = os.system(cmd_str)
-
 p = pstats.Stats(prof_file)
-p.sort_stats('total').print_stats(num_lines)
+p.sort_stats('t').print_stats(num_lines)
