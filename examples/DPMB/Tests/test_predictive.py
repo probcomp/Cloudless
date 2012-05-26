e5042fdd34e73dd9a11038727dd863cfc2d9ead5
dan lovell <dlovell@gmail.com>
2012-05-26 12:39:48 -0400

    fix default pkl_file_str to be homedir, more likely to be writeable add remote option

--- examples/DPMB/Tests/test_predictive.py
+++ examples/DPMB/Tests/test_predictive.py
@@ -7,6 +7,7 @@ import datetime
 import re
 import gc
 import argparse
+import os
 #
 import Cloudless
 reload(Cloudless)
@@ -14,24 +15,27 @@ import Cloudless.memo
 reload(Cloudless.memo)
 import Cloudless.examples.DPMB.remote_functions as rf
 reload(rf)
-#
-if False:
-    Cloudless.base.remote_mode()
-    Cloudless.base.remote_exec('import Cloudless.examples.DPMB_remote_functions as rf')
-    Cloudless.base.remote_exec('reload(rf)')
 
+
+# load up some arguments
 parser = argparse.ArgumentParser(description='A test run that plots predictive, among other things')
 parser.add_argument('--num_rows',default=64*64,type=int)
 parser.add_argument('--num_clusters',default=64,type=int)
 parser.add_argument('--num_iters',default=1000,type=int)
 parser.add_argument('--num_nodes',default=5,type=int)
 parser.add_argument('--time_seatbelt',default=60,type=int)
+parser.add_argument('--pkl_file_str',default=os.path.expanduser("~/test_predictive_pickled_jobs.pkl"),type=str)
+parser.add_argument('--remote',action='store_true')
+#
 args = parser.parse_args()
 
-pkl_file_str = "test_predictive_pickled_jobs.pkl"
 
-ALL_RUN_SPECS = []
-#
+if args.remote:
+    Cloudless.base.remote_mode()
+    Cloudless.base.remote_exec('import Cloudless.examples.DPMB_remote_functions as rf')
+    Cloudless.base.remote_exec('reload(rf)')
+
+
 run_spec = rf.gen_default_run_spec()
 run_spec["dataset_spec"]["num_rows"] = args.num_rows
 run_spec["dataset_spec"]["gen_z"] = ("balanced",args.num_clusters)
@@ -49,4 +53,4 @@ print "Created memoizer"
 memoized_infer(run_spec)
 
 rf.try_plots(memoized_infer,which_measurements=["predictive","ari","num_clusters","score"])
-rf.pickle_if_done(memoized_infer,file_str=pkl_file_str)
+rf.pickle_if_done(memoized_infer,file_str=args.pkl_file_str)
