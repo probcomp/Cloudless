96b7fd326ce756d3897d5ae797a35a89f5c873bc
Dan Lovell <dlovell@gmail.com>
2012-06-08 13:12:45 -0400

    create test_gibbs_init to (weakly) test old initialization as well as new gibbs-type initialization

---
+++ examples/DPMB/Tests/test_gibbs_init.py
@@ -0,0 +1,50 @@
+import numpy as np
+#
+import Cloudless.examples.DPMB.DPMB as dm
+reload(dm)
+import Cloudless.examples.DPMB.DPMB_State as ds
+reload(ds)
+
+num_cols = 16
+num_rows = 16
+
+state_0 = ds.DPMB_State(
+    gen_seed = 0
+    , num_cols = num_cols
+    , num_rows = num_rows
+)
+state_0.plot(save_str="random_state")
+
+state_1 = ds.DPMB_State(
+    gen_seed = 0
+    , num_cols = num_cols
+    , num_rows = num_rows
+    , init_alpha = 1
+    , init_betas = None
+    , init_z = ("balanced",4)
+)
+state_1.plot(save_str="balanced_state")
+
+init_x = state_1.getXValues()
+state_2 = ds.DPMB_State(
+    gen_seed = 0
+    , num_cols = num_cols
+    , num_rows = num_rows
+    , init_alpha = 1
+    , init_betas = np.repeat(.1,num_cols)
+    , init_z = None
+    , init_x = init_x
+)
+state_2.plot(save_str="gibbs_init")
+
+init_x_permuted = np.random.permutation(init_x)
+state_3 = ds.DPMB_State(
+    gen_seed = 0
+    , num_cols = num_cols
+    , num_rows = num_rows
+    , init_alpha = 1
+    , init_betas = np.repeat(.1,num_cols)
+    , init_z = None
+    , init_x = init_x_permuted
+)
+state_3.plot(save_str="gibbs_init_permuted")
