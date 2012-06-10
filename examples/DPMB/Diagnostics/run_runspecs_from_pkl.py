21d7ecffcc73aa677429254cfb24f1f8327d2512
dan lovell <dlovell@gmail.com>
2012-06-10 10:27:50 -0400

    only modify time_seatbelt if argument passed add helper functions for quicker operations

--- examples/DPMB/Diagnostics/run_runspecs_from_pkl.py
+++ examples/DPMB/Diagnostics/run_runspecs_from_pkl.py
@@ -16,8 +16,8 @@ reload(rf)
 # load up some arguments
 parser = argparse.ArgumentParser(description='Run ALL_RUN_SPECS from a pkl file')
 parser.add_argument('runspec_file_str',type=str)
-parser.add_argument('--time_seatbelt',default=300,type=int)
-parser.add_argument('--pkl_file_str',default=os.path.expanduser("~/run_from_pkl_pickled_jobs.pkl"),type=str)
+parser.add_argument('--time_seatbelt',default=-1,type=int)
+parser.add_argument('--pkl_file_str',default=os.path.expanduser("~/run_from_pkl_pickled_jobs.pkl.gz"),type=str)
 parser.add_argument('--infer_name',default="infer",type=str)
 parser.add_argument('--not_remote',action='store_true')
 args = parser.parse_args()
@@ -50,8 +50,20 @@ memoized_infer = Cloudless.memo.AsyncMemoize(
 print "Created memoizer"
 
 for run_spec in ALL_RUN_SPECS:
-    run_spec["time_seatbelt"] = time_seatbelt
+    if time_seatbelt != -1:
+        run_spec["time_seatbelt"] = time_seatbelt
     memoized_infer(run_spec)
 
-# rf.try_plots(memoized_infer,which_measurements=which_measurements)
-# rf.pickle_if_done(memoized_infer,file_str=pkl_file_str)
+
+def report():
+    memoized_infer.report_status()
+
+def plot():
+    rf.try_plots(memoized_infer,which_measurements=which_measurements)
+
+def pickle():
+    memoized_infer.advance()
+    rf.pickle_asyncmemoize(memoized_infer,file_str=pkl_file_str)
+
+def pickle_if_done():
+    rf.pickle_if_done(memoized_infer,file_str=pkl_file_str)
