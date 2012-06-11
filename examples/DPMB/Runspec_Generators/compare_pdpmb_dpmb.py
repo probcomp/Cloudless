fb30ec02e3456c905c22ffceb6a2918f7793dcbf
dan lovell <dlovell@gmail.com>
2012-06-11 11:31:35 -0400

    change run_spec['verbose_state'] to False

--- examples/DPMB/Runspec_Generators/compare_pdpmb_dpmb.py
+++ examples/DPMB/Runspec_Generators/compare_pdpmb_dpmb.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 NUM_CLUSTERS = 4
-VECTORS_PER_CLUSTER = 32
+VECTORS_PER_CLUSTER = 128
 beta_d = 1.0
 def gen_default_run_spec():
     dataset_spec = {}
     dataset_spec["gen_seed"] = 0
-    dataset_spec["num_cols"] = 16
+    dataset_spec["num_cols"] = 8
     dataset_spec["num_rows"] = NUM_CLUSTERS*VECTORS_PER_CLUSTER
     dataset_spec["gen_alpha"] = 3.0 #FIXME: could make it MLE alpha later
     dataset_spec["gen_betas"] = np.repeat(beta_d, dataset_spec["num_cols"])
@@ -15,7 +15,7 @@ def gen_default_run_spec():
     #
     run_spec = {}
     run_spec["dataset_spec"] = dataset_spec
-    run_spec["num_iters"] = 100
+    run_spec["num_iters"] = 10
     run_spec["num_nodes"] = 1
     run_spec["infer_seed"] = 0
     run_spec["infer_init_alpha"] = 3.0
@@ -24,13 +24,13 @@ def gen_default_run_spec():
     run_spec["infer_do_betas_inference"] = True
     run_spec["infer_init_z"] = None
     run_spec["hypers_every_N"] = 1
-    run_spec["time_seatbelt"] = 30
+    run_spec["time_seatbelt"] = 60
     run_spec["ari_seatbelt"] = None
-    run_spec["verbose_state"] = True # FIXME : change back when done debugging
+    run_spec["verbose_state"] = False
     #
     return run_spec
 
-NUM_RUNS = 1
+NUM_RUNS = 3
 NUM_DATASETS = 3
 NUM_NODES_LIST = [1] # [1,4,16]
 HYPERS_EVERY_N_LIST = [4,16]
