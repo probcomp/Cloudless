f995b1043fbe1d3f641069fc0d58ca94ff31c85d
dan lovell <dlovell@gmail.com>
2012-06-13 16:33:03 -0400

    save plots to save_dir argument

--- examples/DPMB/run_runspecs_from_pkl.py
+++ examples/DPMB/run_runspecs_from_pkl.py
@@ -13,22 +13,27 @@ reload(Cloudless.memo)
 import Cloudless.examples.DPMB.remote_functions as rf
 reload(rf)
 
+default_save_dir = os.path.expanduser("~/Run/")
+default_pkl_file_str = os.path.join(
+    default_save_dir,"run_from_pkl_pickled_jobs.pkl.gz")
 # load up some arguments
 parser = argparse.ArgumentParser(description='Run ALL_RUN_SPECS from a pkl file')
 parser.add_argument('runspec_file_str',type=str)
 parser.add_argument('--time_seatbelt',default=-1,type=int)
-parser.add_argument('--pkl_file_str',default=os.path.expanduser("~/run_from_pkl_pickled_jobs.pkl.gz"),type=str)
+parser.add_argument('--save_dir',default=default_save_dir,type=str)
+parser.add_argument('--pkl_file_str',default=default_pkl_file_str,type=str)
 parser.add_argument('--infer_name',default="infer",type=str)
 parser.add_argument('--infer_func_str',default="rf.infer",type=str)
 parser.add_argument('--not_remote',action='store_true')
 args = parser.parse_args()
 #
 runspec_file_str = args.runspec_file_str
+time_seatbelt = args.time_seatbelt
+save_dir = args.save_dir
 pkl_file_str = args.pkl_file_str
 infer_name = args.infer_name
-time_seatbelt = args.time_seatbelt
-remote = not args.not_remote
 infer_func = eval(args.infer_func_str)
+remote = not args.not_remote
 
 try:
     with open(runspec_file_str,"rb") as fh:
@@ -36,6 +41,7 @@ try:
 except Exception, e:
     print "Couldn't load ALL_RUN_SPECS from " + runspec_file_str
     print str(e)
+    exit()
 
 if remote:
     Cloudless.base.remote_mode()
@@ -62,7 +68,11 @@ def report():
 
 def plot():
     which_measurements=["predictive","ari","num_clusters","score"]
-    rf.try_plots(memoized_infer,which_measurements=which_measurements)
+    rf.try_plots(
+        memoized_infer,
+        which_measurements=which_measurements,
+        save_dir=save_dir,
+        )
 
 def pickle():
     memoized_infer.advance()
