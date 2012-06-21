83dbc504ddadf7a5a75974090453b206df4d4471
Dan Lovell <dlovell@gmail.com>
2012-06-21 11:09:19 -0400

    windows needs to know which python to execute cython.py with

--- examples/DPMB/compile_pyx_functions.sh
+++ examples/DPMB/compile_pyx_functions.sh
@@ -1,5 +1,6 @@
+#!bash
 if [[ $(uname) != "Linux" ]] ; then 
-    c:/Python27/Scripts/cython.py -a pyx_functions.pyx
+    c:/Python27/python c:/Python27/Scripts/cython.py -a pyx_functions.pyx
     g++ -o pyx_functions.pyd -shared --verbose -fwrapv -O2 -Wall -fno-strict-aliasing -Ic:/Python27/include -Ic:/Python27/Lib/site-packages/numpy/core/include/ pyx_functions.c c:/Python27/python27.dll
 else 
     cython -a pyx_functions.pyx
