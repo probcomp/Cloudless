8c67be8b6c0f553b1439bb0463fadec7b9bb1de1
dan lovell <dlovell@gmail.com>
2012-06-20 23:12:34 -0400

    name cifar problem files remove unused settings

--- examples/DPMB/settings.py
+++ examples/DPMB/settings.py
@@ -1,30 +1,17 @@
 #!python
 import os
 
-
+# resolve directories to use
 if os.sys.platform in {'cygwin':None,"win32":None}:
     root_dir = "c:" + os.path.sep
 else:
     root_dir = os.path.join(os.path.sep,"usr","local")
 data_dir = os.path.join(root_dir,"Cloudless","examples","DPMB","Data")
 
-##data dimensions
-numColumns = 50
-numVectors = 2000
-
-##priors
-gamma_k = 1 
-gamma_theta = 1
-
-##computation
-nGibbsSteps = 3
-
-##control
-clipBeta = [1E-2,1E10]
-infer_alpha = None
-infer_beta = None
-verbose = True
-
-##os
+# gdocs settings
 auth_file = os.path.expanduser("~/mh_gdocs_auth")
 gdocs_folder_default = "MH"
+
+# cifar
+cifar_10_problem_file = "cifar_10_problem.pkl"
+cifar_100_problem_file = "cifar_100_problem.pkl"
