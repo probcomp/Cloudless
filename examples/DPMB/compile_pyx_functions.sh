66e8459d38088c4153988305fc2f385669a258e0
Dan Lovell <dlovell@gmail.com>
2012-06-21 11:12:19 -0400

    cd into the appropriate directory, this is probably a little brittle

--- examples/DPMB/compile_pyx_functions.sh
+++ examples/DPMB/compile_pyx_functions.sh
@@ -1,8 +1,10 @@
 #!bash
 if [[ $(uname) != "Linux" ]] ; then 
+    cd c:/Cloudless/examples/DPMB/
     c:/Python27/python c:/Python27/Scripts/cython.py -a pyx_functions.pyx
     g++ -o pyx_functions.pyd -shared --verbose -fwrapv -O2 -Wall -fno-strict-aliasing -Ic:/Python27/include -Ic:/Python27/Lib/site-packages/numpy/core/include/ pyx_functions.c c:/Python27/python27.dll
 else 
+    cd /usr/local/Cloudless/examples/DPMB/
     cython -a pyx_functions.pyx
     gcc -o pyx_functions.so -shared -pthread -I/usr/include/python2.7 pyx_functions.c
 fi
