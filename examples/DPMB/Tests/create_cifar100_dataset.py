4f2b58c05b19e832d666e1f56ecfa2f0687a68e5
dan lovell <dlovell@gmail.com>
2012-06-20 23:03:24 -0400

    include test data in problem pkl files

--- examples/DPMB/Tests/create_cifar100_dataset.py
+++ examples/DPMB/Tests/create_cifar100_dataset.py
@@ -9,27 +9,41 @@ import numpy
 #
 import Cloudless.examples.DPMB.helper_functions as hf
 reload(hf)
+import Cloudless.examples.DPMB.settings as settings
+reload(settings)
+
 
 # read in labels from alex's data
-path = os.path.join("Data","cifar-100-python")
+path = os.path.join(settings.data_dir,"cifar-100-python")
+# read the training data
 filename = "train"
 full_filename = os.path.join(path,filename)
 with open(full_filename) as fh:
     cifar = cPickle.load(fh)
-labels = numpy.array(cifar["coarse_labels"])
+train_labels = numpy.array(cifar["coarse_labels"])
+# read the test data
+filename = "test"
+full_filename = os.path.join(path,filename)
+with open(full_filename) as fh:
+    cifar = cPickle.load(fh)
+test_labels = numpy.array(cifar["coarse_labels"])
 
 # read in rpa datavectors
-filename = os.path.join("Data","CIFAR100-codes.pkl")
+filename = os.path.join(settings.data_dir,"CIFAR100-codes.pkl")
 with open(filename) as fh:
   cifar = cPickle.load(fh)
-data = hf.convert_rpa_representation(cifar["codes"][:len(labels)])
+train_data = hf.convert_rpa_representation(cifar["codes"][:len(train_labels)])
+test_data = hf.convert_rpa_representation(cifar["codes"][-len(test_labels):])
 
 # write out to a pickle file
 filename = "cifar_100_problem.pkl"
 cifar = {
-    "zs":labels,
-    "xs":data,
+    "zs":train_labels,
+    "xs":train_data,
+    "test_zs":test_labels,
+    "test_xs":test_data,
     "name":"cifar-100",
+    "type":"coarse_labels"
 }
 with open(filename,"wb") as fh:
     cPickle.dump(cifar,fh)
--- examples/DPMB/Tests/create_cifar10_dataset.py
+++ examples/DPMB/Tests/create_cifar10_dataset.py
@@ -9,31 +9,43 @@ import numpy
 #
 import Cloudless.examples.DPMB.helper_functions as hf
 reload(hf)
+import Cloudless.examples.DPMB.settings as settings
+reload(settings)
+
 
 # read in labels from alex's data
-path = os.path.join("Data","cifar-10-batches-py")
-# filename = "batches.meta"
+path = os.path.join(settings.data_dir,"cifar-10-batches-py")
+# read the train labels
 filenames = ["data_batch_1","data_batch_2","data_batch_3","data_batch_4","data_batch_5"]
-labels_list = []
+train_labels_list = []
 for filename in filenames:
     full_filename = os.path.join(path,filename)
     with open(full_filename) as fh:
         cifar = cPickle.load(fh)
-    labels_list.append(cifar["labels"])
-#
-labels = numpy.array(labels_list).flatten()
+    train_labels_list.append(cifar["labels"])
+train_labels = numpy.array(train_labels_list).flatten()
+# read the test labels
+filename = "test_batch"
+full_filename = os.path.join(path,filename)
+with open(full_filename) as fh:
+    cifar = cPickle.load(fh)
+test_labels= numpy.array(cifar["labels"])
+
 
 # read in rpa datavectors
-filename = os.path.join("Data","CIFAR10-codes.pkl")
+filename = os.path.join(settings.data_dir,"CIFAR10-codes.pkl")
 with open(filename) as fh:
   cifar = cPickle.load(fh)
-data = hf.convert_rpa_representation(cifar["codes"][:len(labels)])
+train_data = hf.convert_rpa_representation(cifar["codes"][:len(train_labels)])
+test_data = hf.convert_rpa_representation(cifar["codes"][-len(test_labels):])
 
 # write out to a pickle file
 filename = "cifar_10_problem.pkl"
 cifar = {
-    "zs":labels,
-    "xs":data,
+    "zs":train_labels,
+    "xs":train_data,
+    "test_zs":test_labels,
+    "test_xs":test_data,
     "name":"cifar-10",
 }
 with open(filename,"wb") as fh:
