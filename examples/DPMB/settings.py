849573bef3a1c0c872fc4a512a537a395e847abc
Dan Lovell <dlovell@gmail.com>
2012-06-22 12:58:05 -0400

    fix AWS root_dir handling

--- examples/DPMB/settings.py
+++ examples/DPMB/settings.py
@@ -11,6 +11,11 @@ else:
     root_dir = os.path.join(os.path.sep,"usr","local")
     is_window = False
 base_dir = os.path.join(root_dir,"Cloudless","examples","DPMB")
+
+if not os.path.isdir(base_dir): # assume AWS
+    root_dir = os.path.join(os.path.sep,"usr","local","lib","python2.7","dist-packages")
+    base_dir = os.path.join(root_dir,"Cloudless","examples","DPMB")
+
 data_dir = os.path.join(base_dir,"Data")
 
 # compile pyx_functions.pyx
