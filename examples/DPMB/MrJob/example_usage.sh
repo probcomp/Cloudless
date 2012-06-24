8f95f195d32947b644a7dea402b5e82cb7b50e54
dan lovell <dlovell@gmail.com>
2012-06-24 16:32:48 -0400

    demonstration of chained and non-chained jobs

---
+++ examples/DPMB/MrJob/example_usage.sh
@@ -0,0 +1,5 @@
+#! bash
+python seed_inferer.py --num-steps 2 --num-iters 10 < seed_list.txt > output_sequential.txt &
+python seed_inferer.py --num-steps 1 --num-iters 10 < seed_list.txt > output_single.txt &
+wait
+python -i output_reader.py
