36cc79e3989be9cb65ab9678a0bbe505edd549d2
dan lovell <dlovell@gmail.com>
2012-06-24 16:32:24 -0400

    loop through each line in fh

--- examples/DPMB/MrJob/output_reader.py
+++ examples/DPMB/MrJob/output_reader.py
@@ -13,18 +13,16 @@ which_protocol = PickleProtocol
 filename = "output_sequential.txt"
 summaries_sequential = {}
 with open(filename) as fh:
-    line = fh.readline()
-    infer_seed,summaries = which_protocol.read(line)
-    # print infer_seed
-    summaries_sequential[infer_seed] = summaries
+    for line in fh:
+        infer_seed,summaries = which_protocol.read(line)
+        summaries_sequential[infer_seed] = summaries
 
 filename = "output_single.txt"
 summaries_single = {}
 with open(filename) as fh:
-    line = fh.readline()
-    infer_seed,summaries = which_protocol.read(line)
-    # print infer_seed # this is the summary?
-    summaries_single[infer_seed] = summaries
+    for line in fh:
+        infer_seed,summaries = which_protocol.read(line)
+        summaries_single[infer_seed] = summaries
 
 for key in summaries_sequential:
     print [summary['score'] for summary in summaries_sequential[key]]
