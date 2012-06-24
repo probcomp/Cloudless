ac475484e511bdf5262387633ca3eecdb448dd77
dan lovell <dlovell@gmail.com>
2012-06-24 15:46:14 -0400

    example of reading back mrjob output

---
+++ examples/DPMB/MrJob/output_reader.py
@@ -0,0 +1,37 @@
+#!python
+from mrjob.job import MRJob
+from mrjob.protocol import PickleProtocol, RawValueProtocol
+from numpy import *
+#
+import Cloudless.examples.DPMB.remote_functions as rf
+reload(rf)
+import Cloudless.examples.DPMB.settings as settings
+reload(settings)
+
+which_protocol = PickleProtocol
+
+filename = "output_sequential.txt"
+summaries_sequential = {}
+with open(filename) as fh:
+    line = fh.readline()
+    infer_seed,summaries = which_protocol.read(line)
+    # print infer_seed
+    summaries_sequential[infer_seed] = summaries
+
+filename = "output_single.txt"
+summaries_single = {}
+with open(filename) as fh:
+    line = fh.readline()
+    infer_seed,summaries = which_protocol.read(line)
+    # print infer_seed # this is the summary?
+    summaries_single[infer_seed] = summaries
+
+for key in summaries_sequential:
+    print [summary['score'] for summary in summaries_sequential[key]]
+for key in summaries_single:
+    print [summary['score'] for summary in summaries_single[key]]
+print
+for key in summaries_sequential:
+    print [mean(summary['test_lls']) for summary in summaries_sequential[key]]
+for key in summaries_single:
+    print [mean(summary['test_lls']) for summary in summaries_single[key]]
