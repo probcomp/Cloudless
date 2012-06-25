b78b64d6bb241fd36cb82781dd1b73e9509df6d3
dan lovell <dlovell@gmail.com>
2012-06-25 15:11:47 -0400

    each seed value must be followed by a newline, otherwise mrjob chokes on EC2

--- examples/DPMB/MrJob/example_usage.sh
+++ examples/DPMB/MrJob/example_usage.sh
@@ -1,5 +1,5 @@
 #! bash
-printf "1\n2" > seed_list.txt
+printf "1\n2\n" > seed_list.txt
 python seed_inferer.py --num-steps 2 --num-iters 10 < seed_list.txt > output_sequential.txt &
 python seed_inferer.py --num-steps 1 --num-iters 10 < seed_list.txt > output_single.txt &
 wait
