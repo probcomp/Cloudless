e4d4755d750bde1b9a140afd4fa255877667b65d
dan lovell <dlovell@gmail.com>
2012-06-27 15:50:07 -0400

    infer parallel but with one node to test equivalence

--- examples/DPMB/MrJob/example_usage.sh
+++ examples/DPMB/MrJob/example_usage.sh
@@ -1,6 +1,6 @@
 #! bash
 printf "1\n2\n" > seed_list.txt
-python seed_inferer.py --num-steps 2 --num-iters 10 < seed_list.txt > output_sequential.txt &
-python seed_inferer.py --num-steps 1 --num-iters 10 < seed_list.txt > output_single.txt &
+python seed_inferer_parallel.py --num-steps 2 --num-iters 4 --num-nodes 1 < seed_list.txt > output_sequential.txt &
+python seed_inferer_parallel.py --num-steps 1 --num-iters 4 --num-nodes 1 < seed_list.txt > output_single.txt &
 wait
 python -i output_reader.py
