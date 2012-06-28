b356d0af322a629ef7e7609367c7345eded49100
dan lovell <dlovell@gmail.com>
2012-06-28 00:30:01 -0400

    must use -fPIC in compilation for 64 bit EC2 images

--- examples/DPMB/compile_pyx_functions.sh
+++ examples/DPMB/compile_pyx_functions.sh
@@ -10,6 +10,6 @@ if [[ $(uname) != "Linux" ]] ; then
     g++ -o pyx_functions.pyd -shared --verbose -fwrapv -O2 -Wall -fno-strict-aliasing -Ic:/Python27/include -Ic:/Python27/Lib/site-packages/numpy/core/include/ pyx_functions.c c:/Python27/python27.dll
 else 
     cython -a pyx_functions.pyx
-    gcc -o pyx_functions.so -shared -pthread -I/usr/include/python2.7 pyx_functions.c
+    gcc -fPIC -o pyx_functions.so -shared -pthread -I/usr/include/python2.7 pyx_functions.c
 fi
 
