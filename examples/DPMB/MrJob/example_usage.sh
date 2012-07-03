7d533d820320722ded23b6c6a1585b9950ead7b7
dan lovell <dlovell@gmail.com>
2012-07-03 18:13:25 -0400

    make num tasks explicit

--- examples/DPMB/MrJob/example_usage.sh
+++ examples/DPMB/MrJob/example_usage.sh
@@ -1,7 +1,7 @@
 #! bash
 printf "1\n2\n" > seed_list.txt
 printf "using seed_list.txt:\n`cat seed_list.txt`\n"
-python seed_inferer.py --num-steps 2 --num-iters 4 --num-nodes 1 < seed_list.txt > output_sequential.txt &
-python seed_inferer.py --num-steps 1 --num-iters 4 --num-nodes 1 < seed_list.txt > output_single.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=2 --num-steps 2 --num-iters 4 --num-nodes 1 < seed_list.txt > output_sequential.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=2 --num-steps 1 --num-iters 4 --num-nodes 1 < seed_list.txt > output_single.txt &
 wait
 python -i output_reader.py
