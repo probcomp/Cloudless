f36757f82bf5fd83a17df327cfcc01bace0d2dfb
dan lovell <dlovell@gmail.com>
2012-07-05 09:31:10 -0400

    enable creation of balanced data

---
+++ examples/DPMB/Tests/create_structured_data.py
@@ -0,0 +1,164 @@
+#!python
+import argparse
+import sys
+import os
+#
+import matplotlib
+matplotlib.use('Agg')
+import numpy
+import pylab
+#
+import Cloudless.examples.DPMB.DPMB_State as ds
+reload(ds)
+import Cloudless.examples.DPMB.remote_functions as rf
+reload(rf)
+import Cloudless.examples.DPMB.helper_functions as hf
+reload(hf)
+import Cloudless.examples.DPMB.settings as settings
+reload(settings)
+
+
+def gen_data(gen_seed,num_clusters,num_cols,num_rows,beta_d):
+    state = ds.DPMB_State(
+        gen_seed=gen_seed,
+        num_cols=num_cols,
+        num_rows=num_rows,
+        init_z=('balanced',num_clusters),
+        init_betas = numpy.repeat(beta_d,num_cols)
+    )
+    return state.getXValues()
+
+def gen_factorial_data(gen_seed,num_clusters,num_cols,num_rows,num_splits,beta_d,
+                       plot=False,image_save_str=None):
+    numpy.random.seed(gen_seed)
+    data_list = []
+    inverse_permutation_indices_list = []
+    for data_idx in xrange(num_splits):
+        data_i = gen_data(
+            gen_seed=numpy.random.randint(sys.maxint),
+            num_clusters=num_clusters,
+            num_cols=num_cols/num_splits,
+            num_rows=num_rows,
+            beta_d=beta_d
+            )
+        permutation_indices = numpy.random.permutation(xrange(num_rows))
+        inverse_permutation_indices = numpy.argsort(permutation_indices)
+        inverse_permutation_indices_list.append(inverse_permutation_indices)
+        data_list.append(numpy.array(data_i)[permutation_indices])
+    data = numpy.hstack(data_list)
+
+    # this is just to visualize, data is already generated
+    if image_save_str is not None or plot:
+        for state_idx in xrange(num_splits):
+            state = ds.DPMB_State(
+                gen_seed=numpy.random.randint(sys.maxint),
+                num_cols=num_cols,
+                num_rows=num_rows,
+                init_z=('balanced',num_clusters),
+                init_x=data[inverse_permutation_indices_list[state_idx]]
+                )
+            save_str = None
+            if image_save_str is not None:
+                save_str = image_save_str + '_' + str(state_idx)
+            state.plot(save_str=save_str)
+
+            hf.plot_data(data=data[inverse_permutation_indices_list[state_idx]])
+            pylab.savefig('just_state_'+str(state_idx))
+            pylab.close()
+            
+        
+    return data,inverse_permutation_indices_list
+
+def make_balanced_data(gen_seed,num_clusters,num_cols,num_rows,beta_d,
+                       plot=False,image_save_str=None):
+
+    num_splits = 2 # only two splits for now
+    numpy.random.seed(gen_seed)
+    rows_per_cluster = num_rows/num_clusters
+    distribute_indices = numpy.array((numpy.arange(rows_per_cluster)*num_clusters).tolist()*num_clusters) \
+        + numpy.repeat(range(num_clusters),rows_per_cluster)
+    # distribute_indices = (numpy.arange(num_rows) + rows_per_cluster/4) % num_rows
+    inverse_distribute_indices = [
+        range(num_rows),
+        numpy.argsort(distribute_indices)
+        ]
+
+    permutation_indices = numpy.random.permutation(range(num_rows))
+    inverse_distribute_indices = [
+        numpy.argsort(permutation_indices)[indices]
+        for indices in inverse_distribute_indices
+        ]
+
+    data_1 = numpy.array(
+        gen_data(numpy.random.randint(sys.maxint),
+                 num_clusters,num_cols/2,num_rows,beta_d))
+    data_2 = numpy.array(
+        gen_data(numpy.random.randint(sys.maxint),
+                 num_clusters,num_cols/2,num_rows,beta_d))[distribute_indices]
+    data = numpy.hstack([data_1,data_2])[permutation_indices]
+
+    # this is just to visualize, data is already generated
+    if image_save_str is not None or plot:
+        for state_idx in xrange(num_splits):
+            state = ds.DPMB_State(
+                gen_seed=numpy.random.randint(sys.maxint),
+                num_cols=num_cols,
+                num_rows=num_rows,
+                init_z=('balanced',num_clusters),
+                init_x=data[inverse_distribute_indices[state_idx]]
+                )
+            save_str = None
+            if image_save_str is not None:
+                save_str = image_save_str + '_' + str(state_idx)
+            state.plot(save_str=save_str)
+
+            hf.plot_data(data=data[inverse_distribute_indices[state_idx]])
+            pylab.savefig('just_state_'+str(state_idx))
+            pylab.close()
+
+    return data,inverse_distribute_indices
+
+def main():
+    parser = argparse.ArgumentParser('Create a factorial problem')
+    parser.add_argument('gen_seed',type=int)
+    parser.add_argument('num_cols',type=int)
+    parser.add_argument('num_rows',type=int)
+    parser.add_argument('num_clusters',type=int)
+    parser.add_argument('num_splits',type=int)
+    parser.add_argument('--beta_d',default=1.0,type=float)
+    parser.add_argument('--pkl_file',default='factorial_problem.pkl.gz',type=str)
+    parser.add_argument('--image_save_str',default=None,type=str)
+    args,unkown_args = parser.parse_known_args()
+
+    data,inverse_permutation_indices_list = gen_factorial_data(
+        gen_seed=args.gen_seed,
+        num_cols=args.num_cols,
+        num_rows=args.num_rows,
+        num_clusters=args.num_clusters,
+        num_splits=args.num_splits,
+        beta_d=args.beta_d,
+        image_save_str=args.image_save_str)
+    # data,inverse_permutation_indices_list = make_balanced_data(
+    #     gen_seed=args.gen_seed,
+    #     num_cols=args.num_cols,
+    #     num_rows=args.num_rows,
+    #     num_clusters=args.num_clusters,
+    #     beta_d=args.beta_d,
+    #     image_save_str=args.image_save_str)
+
+    pkl_vals = {
+        'data':data,
+        'inverse_permutation_indices_list':inverse_permutation_indices_list,
+        'num_clusters':args.num_clusters,
+        'zs_to_permute':numpy.repeat(xrange(args.num_clusters),
+                                     args.num_rows/args.num_clusters),
+        'beta_d':args.beta_d
+        }
+
+    rf.pickle(
+        pkl_vals,
+        os.path.join(settings.data_dir,args.pkl_file))
+
+if __name__ == '__main__':
+    main()
+
