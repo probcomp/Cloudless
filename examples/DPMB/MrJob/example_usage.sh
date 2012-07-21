64bbbcec729f3fd277db6dd99e0079dc9cb1945f
dan lovell <dlovell@gmail.com>
2012-07-21 11:59:40 -0400

    test several nodes

--- examples/DPMB/MrJob/example_usage.sh
+++ examples/DPMB/MrJob/example_usage.sh
@@ -1,7 +1,9 @@
 #! bash
 printf "1\n2\n" > seed_list.txt
 printf "using seed_list.txt:\n`cat seed_list.txt`\n"
-python seed_inferer.py --jobconf mapred.map.tasks=2 --num-steps 2 --num-iters 4 --num-nodes 1 < seed_list.txt > output_sequential.txt &
-python seed_inferer.py --jobconf mapred.map.tasks=2 --num-steps 1 --num-iters 4 --num-nodes 1 < seed_list.txt > output_single.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=4 --num-steps 2 --num-iters 8 --num-nodes 4 < seed_list.txt > num_nodes_4.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=2 --num-steps 4 --num-iters 8 --num-nodes 2 < seed_list.txt > num_nodes_2.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=1 --num-steps 8 --num-iters 8 --num-nodes 1 < seed_list.txt > num_nodes_1.txt &
+
 wait
 python -i output_reader.py
