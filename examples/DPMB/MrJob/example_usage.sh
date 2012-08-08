f8bec3f7b7fc173d05f4ee4d7d8a80212e1a538f
dan lovell <dlovell@gmail.com>
2012-08-08 10:08:50 -0400

    wrap long lines

--- examples/DPMB/MrJob/example_usage.sh
+++ examples/DPMB/MrJob/example_usage.sh
@@ -1,9 +1,12 @@
 #! bash
 printf "1\n" > seed_list.txt
 printf "using seed_list.txt:\n`cat seed_list.txt`\n"
-python seed_inferer.py --jobconf mapred.map.tasks=4 --num-iters 24 --num-nodes 4 < seed_list.txt > num_nodes_4.txt &
-python seed_inferer.py --jobconf mapred.map.tasks=2 --num-iters 16 --num-nodes 2 < seed_list.txt > num_nodes_2.txt &
-python seed_inferer.py --jobconf mapred.map.tasks=1 --num-iters 8 --num-nodes 1 < seed_list.txt > num_nodes_1.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=4 --num-iters 24 --num-nodes 4 \
+    < seed_list.txt > num_nodes_4.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=2 --num-iters 16 --num-nodes 2 \
+    < seed_list.txt > num_nodes_2.txt &
+python seed_inferer.py --jobconf mapred.map.tasks=1 --num-iters 8 --num-nodes 1 \
+    < seed_list.txt > num_nodes_1.txt &
 
 wait
 python -i consolidate_summaries.py
