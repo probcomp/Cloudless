4e58c8a0ada09e802dead33ef4ae2a8386f6c7fe
dan lovell <dlovell@gmail.com>
2012-08-17 11:23:13 -0400

    modify example_usage for new large problem

--- examples/DPMB/MrJob/example_usage.sh
+++ examples/DPMB/MrJob/example_usage.sh
@@ -1,11 +1,11 @@
 #! bash
 printf "1\n" > seed_list.txt
 printf "using seed_list.txt:\n`cat seed_list.txt`\n"
-python seed_inferer.py --jobconf mapred.map.tasks=4 --num-iters 24 --num-nodes 4 \
+python seed_inferer.py --jobconf mapred.map.tasks=4 --num-iters 18 --num-nodes 4 \
     < seed_list.txt > num_nodes_4.txt &
-python seed_inferer.py --jobconf mapred.map.tasks=2 --num-iters 16 --num-nodes 2 \
+python seed_inferer.py --jobconf mapred.map.tasks=2 --num-iters 6 --num-nodes 2 \
     < seed_list.txt > num_nodes_2.txt &
-python seed_inferer.py --jobconf mapred.map.tasks=1 --num-iters 8 --num-nodes 1 \
+python seed_inferer.py --jobconf mapred.map.tasks=1 --num-iters 2 --num-nodes 1 \
     < seed_list.txt > num_nodes_1.txt &
 
 wait
