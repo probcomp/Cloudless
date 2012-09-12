ed5d0f12e43d7a6614eccfd6c9d3de08301e344e
dlovell <dlovell@gmail.com>
2012-09-12 16:34:15 -0400

    make executable, taking cmomand line arguments for data directories to consolidate

--- examples/DPMB/MrJob/consolidate_summaries.py
+++ examples/DPMB/MrJob/consolidate_summaries.py
@@ -1,10 +1,12 @@
 #!python
+import argparse
 import os
 import re
 #
-from mrjob.job import MRJob
-from mrjob.protocol import PickleProtocol, RawValueProtocol
-from numpy import *
+import matplotlib
+matplotlib.use('Agg')
+import pylab
+import numpy
 #
 import Cloudless.examples.DPMB.remote_functions as rf
 reload(rf)
@@ -56,7 +58,7 @@ extract_score = lambda summaries : [
     for summary in summaries
     ]
 extract_test_lls = lambda summaries : [
-    ('%.2f' % mean(summary['test_lls']))
+    ('%.2f' % numpy.mean(summary['test_lls']))
     for summary in summaries
     ]
 extract_delta_t = process_timing
@@ -75,3 +77,50 @@ def print_info(summaries_dict):
             print filename
             print extract_func(summaries)
         print
+
+def main():
+
+    # parse some args
+    parser = argparse.ArgumentParser('consolidate summaries')
+    parser.add_argument('data_dirs',nargs='+',type=str)
+    args = parser.parse_args()
+    data_dirs = args.data_dirs
+
+    shorten_name = lambda x: x[8:-8]
+
+    # read the summaries
+    summaries_dict = {}
+    for data_dir in data_dirs:
+        summary_names = get_summary_names(data_dir)
+        temp_summaries_dict = get_summaries_dict(summary_names,data_dir)
+        print_info(summaries_dict)
+        summaries_dict.update(temp_summaries_dict)
+
+    # create test_lls plot
+    pylab.figure()
+    for summaries_name, summaries in summaries_dict.iteritems():
+        timing = numpy.array(extract_delta_t(summaries),dtype=float)
+        test_lls = numpy.array(extract_test_lls(summaries),dtype=float)
+        pylab.plot(timing,test_lls)
+    legend_list = map(shorten_name,summaries_dict.keys())
+    pylab.legend(legend_list,loc='lower right')
+    pylab.title('test_lls')
+    pylab.xlabel('time (seconds)')
+    pylab.ylabel('test set mean log likelihood')
+    pylab.savefig('test_lls')
+
+    # create score plot
+    pylab.figure()
+    for summaries_name, summaries in summaries_dict.iteritems():
+        timing = numpy.array(extract_delta_t(summaries),dtype=float)
+        scores = numpy.array(extract_score(summaries),dtype=float)
+        pylab.plot(timing,scores)
+    legend_list = map(shorten_name,summaries_dict.keys())
+    pylab.legend(legend_list,loc='lower right')
+    pylab.title('scores')
+    pylab.xlabel('time (seconds)')
+    pylab.ylabel('model score')
+    pylab.savefig('scores')
+
+if __name__ == '__main__':
+    main()
