22e1946cc34601d8fab64e17bf3d92d96f7e5b3a
dlovell <dlovell@gmail.com>
2012-09-13 08:42:46 -0400

    use random_state in transition_type permutation enable resume of prior run when resume_full_file is defined use helper_functions.Timer to time sections of code

--- examples/DPMB/MrJob/seed_inferer.py
+++ examples/DPMB/MrJob/seed_inferer.py
@@ -19,10 +19,13 @@ import Cloudless.examples.DPMB.settings as settings
 reload(settings)
 
 
-# problem_file = settings.cifar_100_problem_file
-problem_file = settings.tiny_image_problem_file
+# problem_file = settings.tiny_image_problem_file
+problem_file = 'tiny_image_problem_nImages_10000_nPcaTrain_10000.pkl.gz'
+problem_full_file = os.path.join(settings.data_dir, problem_file)
+resume_file = 'summary_numnodes2_seed1_iternum-1.pkl.gz'
+resume_full_file = os.path.join(settings.data_dir, resume_file)
 
-create_pickle_file = lambda num_nodes, seed_str, iter_num : \
+create_pickle_file_str = lambda num_nodes, seed_str, iter_num : \
     '_'.join([
         'summary',
         'numnodes' + str(num_nodes),
@@ -34,7 +37,7 @@ class MRSeedInferer(MRJob):
 
     INPUT_PROTOCOL = RawValueProtocol
     INTERNAL_PROTOCOL = PickleProtocol
-    OUTPUT_PROTOCOL = PickleProtocol # RawValueProtocol # 
+    OUTPUT_PROTOCOL = PickleProtocol
 
     def configure_options(self):
         super(MRSeedInferer, self).configure_options()
@@ -57,27 +60,33 @@ class MRSeedInferer(MRJob):
             if self.num_steps != 0 else 0
 
     def init(self, key, run_key):
+        start_dt = datetime.datetime.now()
         master_infer_seed = int(run_key)
+        num_nodes = self.num_nodes
+        #
         run_spec = rf.gen_default_cifar_run_spec(
             problem_file=problem_file,
             infer_seed=master_infer_seed,
             num_iters=0 # no inference, just init
            )
-        gen_problem_start = datetime.datetime.now()
-        problem = rf.gen_problem(run_spec['dataset_spec'])
-        gen_problem_delta_t = hf.delta_since(gen_problem_start)
-        infer_problem_start = datetime.datetime.now()
-        summaries = rf.infer(run_spec, problem)
-        infer_problem_delta_t = hf.delta_since(infer_problem_start)
+        with hf.Timer('gen_problem') as gen_problem_timer:
+            problem = rf.gen_problem(run_spec['dataset_spec'])
+        with hf.Timer('init/resume') as infer_timer:
+            if resume_full_file:
+                summaries = [rf.unpickle(resume_full_file)]
+            else:
+                summaries = rf.infer(run_spec, problem)
         # pickle up summary
         summary = summaries[-1]
         summary['problem_hexdigest'] = hashlib.sha224(str(problem)).hexdigest()
-        summary['timing'] = {'start_time':datetime.datetime.now(),
-                             'gen_problem_delta_t':gen_problem_delta_t,
-                             'infer_problem_delta_t':infer_problem_delta_t}
+        summary['timing'] = {
+            'start_time':start_dt,
+            'gen_problem_delta_t':gen_problem_timer.elapsed_secs,
+            'infer_problem_delta_t':infer_timer.elapsed_secs,
+            }
         iter_num = 0
         # FIXME : infer will pickle over this
-        pickle_file = create_pickle_file(self.num_nodes, run_key, str(-1))
+        pickle_file = create_pickle_file_str(num_nodes, run_key, str(-1))
         pickle_full_file = os.path.join(settings.data_dir,pickle_file)
         rf.pickle(summary, pickle_full_file)
         # pull out the values to pass on
@@ -92,15 +101,18 @@ class MRSeedInferer(MRJob):
     def distribute_data(self, run_key, yielded_tuple):
         print 'distribute data'
         child_counter = 0
+        num_nodes = self.num_nodes
+        #
         (init_z, master_alpha, betas, master_inf_seed, iter_num) = yielded_tuple
-        # does distribute_data permute random_state?
         node_data_indices, node_zs, child_gen_seed_list, \
             child_inf_seed_list, master_inf_seed = \
             rf.distribute_data(inf_seed=master_inf_seed,
-                               num_nodes=self.num_nodes,
+                               num_nodes=num_nodes,
                                init_z=init_z)
         for x_indices, zs, child_gen_seed, child_inf_seed in zip(
-            node_data_indices, node_zs, child_gen_seed_list, child_inf_seed_list):
+            node_data_indices, node_zs, child_gen_seed_list,
+            child_inf_seed_list):
+
             if len(zs) == 0:
                 continue
             yielded_tuple = (x_indices, zs, child_gen_seed, child_inf_seed,
@@ -111,12 +123,15 @@ class MRSeedInferer(MRJob):
 
     def infer(self, run_key, model_specs):
         print 'infer'
+        num_nodes = self.num_nodes
+        #
         x_indices, zs, child_gen_seed, child_inf_seed, \
-                   master_alpha, betas, master_inf_seed, iter_num, child_counter \
+                   master_alpha, betas, master_inf_seed, \
+                   iter_num, child_counter \
                    = model_specs
         run_spec = rf.run_spec_from_model_specs(model_specs, self)
         # FIXME : Perhaps problem can be generated in run_spec_from_model_specs?
-        orig_problem = rf.unpickle(os.path.join(settings.data_dir, problem_file))
+        orig_problem = rf.unpickle(problem_full_file)
         problem_xs = numpy.array(orig_problem['xs'],dtype=numpy.int32)
         init_x = [
             orig_problem['xs'][x_index]
@@ -124,23 +139,21 @@ class MRSeedInferer(MRJob):
             ]
         sub_problem = {'xs':init_x,'zs':zs,'test_xs':None}
         # actually infer
+        get_child_pkl_file = lambda child_iter_num: create_pickle_file_str(
+            num_nodes, run_key+'_child'+str(child_counter), child_iter_num)
         child_summaries = None
-        if self.num_nodes == 1:
-            # FIXME : can I just modify run_spec and use rf.infer
-            # FIXME: only distinction is how frequently summaries are pickled
+        if num_nodes == 1:
+            # FIXME : would be nice if intermediate results were pickled
+            # FIXME : else, no way to tell how much progress has been made
             sub_problem['test_xs'] = \
                 numpy.array(orig_problem['test_xs'],dtype=numpy.int32)
             run_spec['infer_do_alpha_inference'] = True
             run_spec['infer_do_betas_inference'] = True
             child_summaries = rf.infer(run_spec, sub_problem)
             for child_iter_num,child_summary in enumerate(child_summaries):
-                pickle_str = os.path.join(
-                    settings.data_dir,
-                    create_pickle_file(
-                        self.num_nodes, run_key+'_child'+str(child_counter),
-                        child_iter_num)
-                    )
-                rf.pickle(child_summary, pickle_str)
+                pkl_file = get_child_pkl_file(child_iter_num)
+                pkl_full_file = os.path.join(settings.data_dir, pkl_file)
+                rf.pickle(child_summary, pkl_full_file)
         else:
             child_summaries = rf.infer(run_spec, sub_problem)
         #
@@ -156,8 +169,10 @@ class MRSeedInferer(MRJob):
     def consolidate_data(self, run_key, yielded_val_generator):
         print 'consolidate data'
         start_dt = datetime.datetime.now()
+        num_nodes = self.num_nodes
         zs_list = []
         x_indices_list = []
+        #
         for (child_zs, child_x_indices, master_alpha, betas,
              master_inf_seed, iter_num) in \
                 yielded_val_generator:
@@ -171,11 +186,10 @@ class MRSeedInferer(MRJob):
             for reorder_index in numpy.argsort(x_indices)
             ]
         #
-        problem = rf.unpickle(os.path.join(settings.data_dir, problem_file))
+        problem = rf.unpickle(problem_full_file)
         init_x = numpy.array(problem['xs'],dtype=numpy.int32)
-        # true_zs,cluster_idx = hf.canonicalize_list(cifar['zs'])
         true_zs, cluster_idx = None, None
-        if problem.get('zs',None) is not None:
+        if 'zs' in  problem:
             true_zs, cluster_idx = hf.canonicalize_list(problem['zs'])
         test_xs = numpy.array(problem['test_xs'],dtype=numpy.int32)
         consolidated_state = ds.DPMB_State(
@@ -190,26 +204,26 @@ class MRSeedInferer(MRJob):
         consolidate_delta = hf.delta_since(start_dt)
         transitioner = dm.DPMB(master_inf_seed, consolidated_state,
                                infer_alpha=True, infer_beta=True)
-        for transition_type in numpy.random.permutation([
-                transitioner.transition_beta, transitioner.transition_alpha]):
+        # FIXME: is this right?
+        random_state = hf.generate_random_state(master_inf_seed)
+        transition_types = [
+            transitioner.transition_beta, transitioner.transition_alpha]
+        for transition_type in random_state.permutation(transition_types):
             transition_type()
-        start_dt = datetime.datetime.now()
-        summary = transitioner.extract_state_summary(
-            true_zs=true_zs,
-            test_xs=test_xs)
-        score_delta = hf.delta_since(start_dt)
+        with hf.Timer('score_delta') as score_delta_timer:
+            summary = transitioner.extract_state_summary(
+                true_zs=true_zs,
+                test_xs=test_xs)
         summary['last_valid_zs'] = transitioner.state.getZIndices()
         summary['timing'] = {
             'timestamp':datetime.datetime.now(),
             'consolidate_delta':consolidate_delta,
-            'score_delta':score_delta
+            'score_delta':score_delta_timer.elapsed_secs,
             }
         #
-        pickle_str = os.path.join(
-            settings.data_dir,
-            create_pickle_file(self.num_nodes, run_key, iter_num)
-            )
-        rf.pickle(summary, pickle_str)
+        pkl_file = create_pickle_file_str(num_nodes, run_key, iter_num)
+        pkl_full_file = os.path.join(settings.data_dir, pkl_file)
+        rf.pickle(summary, pkl_full_file)
         #
         last_valid_zs = summary['last_valid_zs']
         master_alpha = summary['alpha']
@@ -220,13 +234,13 @@ class MRSeedInferer(MRJob):
         yield run_key, yielded_tuple
 
     def steps(self):
-        ret_list = [self.mr(self.init)]
+        step_list = [self.mr(self.init)]
         infer_step = [
             self.mr(self.distribute_data),
-            self.mr(self.infer, self.consolidate_data)
+            self.mr(self.infer, self.consolidate_data),
             ]
-        ret_list.extend(infer_step * self.num_steps)
-        return ret_list
+        step_list.extend(infer_step * self.num_steps)
+        return step_list
 
 if __name__ == '__main__':
     MRSeedInferer.run()
