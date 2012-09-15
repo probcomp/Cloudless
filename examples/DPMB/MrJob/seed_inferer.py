efcb98ec9474dd1f0711242e8fef26eadb33e26c
dlovell <dlovell@gmail.com>
2012-09-14 23:44:33 -0400

    swap of child gen,inf seed generation, causes change of score reordering of variable settings, addition of space after commas

--- examples/DPMB/MrJob/seed_inferer.py
+++ examples/DPMB/MrJob/seed_inferer.py
@@ -54,9 +54,9 @@ class MRSeedInferer(MRJob):
 
     def configure_options(self):
         super(MRSeedInferer, self).configure_options()
-        self.add_passthrough_option('--num-steps',type='int',default=None)
-        self.add_passthrough_option('--num-iters',type='int',default=8)
-        self.add_passthrough_option('--num-nodes',type='int',default=4)
+        self.add_passthrough_option('--num-steps', type='int', default=None)
+        self.add_passthrough_option('--num-iters', type='int', default=8)
+        self.add_passthrough_option('--num-nodes', type='int', default=4)
 
     def load_options(self, args):
         super(MRSeedInferer, self).load_options(args=args)
@@ -77,64 +77,62 @@ class MRSeedInferer(MRJob):
         master_infer_seed = int(run_key)
         num_nodes = self.num_nodes
         #
-        run_spec = rf.gen_default_cifar_run_spec(
-            problem_file=problem_file,
-            infer_seed=master_infer_seed,
-            num_iters=0 # no inference, just init
-           )
         problem_hexdigest = None
         with hf.Timer('init/resume') as init_resume_timer:
             if resume_file:
-                summaries = [rf.unpickle(resume_file, dir=data_dir)]
+                summary = rf.unpickle(resume_file, dir=data_dir)
             else:
+                run_spec = rf.gen_default_cifar_run_spec(
+                    problem_file=problem_file,
+                    infer_seed=master_infer_seed,
+                    num_iters=0 # no inference, just init
+                   )
                 # FIXME: should I pass permute=False here?
                 problem = rf.gen_problem(run_spec['dataset_spec'])
-                summaries = rf.infer(run_spec, problem)
+                summary = rf.infer(run_spec, problem)[-1]
                 problem_hexdigest = get_hexdigest(problem)
-        # pickle up summary
-        summary = summaries[-1]
         summary['problem_hexdigest'] = problem_hexdigest
         summary['timing'] = {
             'start_time':start_dt,
             'infer_problem_delta_t':init_resume_timer.elapsed_secs,
             }
-        iter_num = summary.get('iter_num', 0)
         # FIXME : infer will pickle over this
         pickle_file = create_pickle_file_str(num_nodes, run_key, str(-1))
         rf.pickle(summary, pickle_file, dir=data_dir)
         # pull out the values to pass on
-        last_valid_zs = summary.get('last_valid_zs',summary.get('zs',None))
-        list_of_x_indices = \
-            summary.get('last_valid_list_of_x_indices', None)
+        list_of_x_indices = summary.get('last_valid_list_of_x_indices', None)
+        last_valid_zs = summary.get('last_valid_zs', summary.get('zs', None))
         master_alpha = summary['alpha']
         betas = summary['betas']
         master_inf_seed = summary['inf_seed']
+        iter_num = summary.get('iter_num', 0)
+        #
         master_state = master_state_tuple(
             list_of_x_indices, last_valid_zs, master_alpha, betas,
-            master_inf_seed=master_inf_seed, iter_num=iter_num)
+            master_inf_seed, iter_num)
         yield run_key, master_state
 
     def distribute_data(self, run_key, master_state):
         print 'distribute data'
         child_counter = 0
         num_nodes = self.num_nodes
-        #
+        # pull variables out of master_state
+        list_of_x_indices = master_state.list_of_x_indices
         init_z = master_state.last_valid_zs
         master_alpha = master_state.master_alpha
         betas = master_state.betas
         master_inf_seed = master_state.master_inf_seed
         iter_num = master_state.iter_num
-        list_of_x_indices = master_state.list_of_x_indices
-        #
+        # generate child state info
         num_clusters = len(list_of_x_indices)
         cluster_dest_nodes, random_state = rf.gen_cluster_dest_nodes(
             master_inf_seed, num_nodes, num_clusters)
-        gen_seed_list = [int(x) for x in random_state.tomaxint(num_nodes)]
         inf_seed_list = [int(x) for x in random_state.tomaxint(num_nodes)]
-        #
+        gen_seed_list = [int(x) for x in random_state.tomaxint(num_nodes)]
+        # actually distribute
         child_counter = 0
-        for cluster_indices, child_gen_seed, child_inf_seed in \
-                zip(cluster_dest_nodes, gen_seed_list, inf_seed_list):
+        for cluster_indices, child_inf_seed, child_gen_seed in \
+                zip(cluster_dest_nodes, inf_seed_list, gen_seed_list):
             if len(cluster_indices) == 0: continue # empty node
             child_list_of_x_indices = \
                 [list_of_x_indices[idx] for idx in cluster_indices]
@@ -166,12 +164,12 @@ class MRSeedInferer(MRJob):
             child_state_in, self.num_iters_per_step, self.num_nodes)
         # FIXME : Perhaps problem can be generated in run_spec_from_model_specs?
         orig_problem = rf.unpickle(problem_file, dir=data_dir)
-        problem_xs = numpy.array(orig_problem['xs'],dtype=numpy.int32)
+        problem_xs = numpy.array(orig_problem['xs'], dtype=numpy.int32)
         init_x = [
             orig_problem['xs'][x_index]
             for x_index in x_indices
             ]
-        sub_problem = {'xs':init_x,'zs':zs,'test_xs':None}
+        sub_problem = {'xs':init_x, 'zs':zs, 'test_xs':None}
         # actually infer
         get_child_pkl_file = lambda child_iter_num: create_pickle_file_str(
             num_nodes, run_key+'_child'+str(child_counter), child_iter_num)
@@ -180,11 +178,11 @@ class MRSeedInferer(MRJob):
             # FIXME : would be nice if intermediate results were pickled
             # FIXME : else, no way to tell how much progress has been made
             sub_problem['test_xs'] = \
-                numpy.array(orig_problem['test_xs'],dtype=numpy.int32)
+                numpy.array(orig_problem['test_xs'], dtype=numpy.int32)
             run_spec['infer_do_alpha_inference'] = True
             run_spec['infer_do_betas_inference'] = True
             child_summaries = rf.infer(run_spec, sub_problem, send_zs=True)
-            for child_iter_num,child_summary in enumerate(child_summaries):
+            for child_iter_num, child_summary in enumerate(child_summaries):
                 pkl_file = get_child_pkl_file(child_iter_num)
                 rf.pickle(child_summary, pkl_file, dir=data_dir)
         else:
@@ -233,7 +231,7 @@ class MRSeedInferer(MRJob):
             ]
         #
         problem = rf.unpickle(problem_file, dir=data_dir)
-        init_x = numpy.array(problem['xs'],dtype=numpy.int32)
+        init_x = numpy.array(problem['xs'], dtype=numpy.int32)
         true_zs, cluster_idx = None, None
         if 'zs' in  problem:
             true_zs, cluster_idx = hf.canonicalize_list(problem['zs'])
@@ -241,7 +239,7 @@ class MRSeedInferer(MRJob):
         # FIXME: is this (NEW) canonicaliziation necessary?
         zs, cluster_idx = hf.canonicalize_list(zs) # FIXME
 
-        test_xs = numpy.array(problem['test_xs'],dtype=numpy.int32)
+        test_xs = numpy.array(problem['test_xs'], dtype=numpy.int32)
         consolidated_state = ds.DPMB_State(
             gen_seed=0, # FIXME : random variate from master_inf_seed?
             num_cols=len(betas),
