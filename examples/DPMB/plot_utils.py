d1c788f84db70d5b45cfebdc0d9dec104a4a4903
dlovell <dlovell@gmail.com>
2012-09-17 16:45:16 -0400

    add get_gridspec to help subplotting

--- examples/DPMB/plot_utils.py
+++ examples/DPMB/plot_utils.py
@@ -1,15 +1,24 @@
-import numpy as np
+import numpy
 import scipy.special as ss
+import matplotlib.gridspec as gridspec
 #
 import DPMB as dm
 reload(dm)
 import DPMB_State as ds
 reload(ds)
+
+
+
+def get_gridspec(height_ratios_or_int):
+    height_ratios = height_ratios_or_int
+    if isinstance(height_ratios, int):
+        height_ratios = numpy.repeat(1./height_ratios, height_ratios)
+    return gridspec.GridSpec(len(height_ratios), 1, height_ratios=height_ratios)
     
 def visualize_mle_alpha(cluster_list=None,points_per_cluster_list=None,max_alpha=None):
     import pylab
-    cluster_list = cluster_list if cluster_list is not None else 10**np.arange(0,4,.5)
-    points_per_cluster_list = points_per_cluster_list if points_per_cluster_list is not None else 10**np.arange(0,4,.5)
+    cluster_list = cluster_list if cluster_list is not None else 10**numpy.arange(0,4,.5)
+    points_per_cluster_list = points_per_cluster_list if points_per_cluster_list is not None else 10**numpy.arange(0,4,.5)
     max_alpha = max_alpha if max_alpha is not None else int(1E4)
     ##
     mle_vals = []
@@ -17,7 +26,7 @@ def visualize_mle_alpha(cluster_list=None,points_per_cluster_list=None,max_alpha
         for points_per_cluster in points_per_cluster_list:
             mle_vals.append([clusters,points_per_cluster,dm.mle_alpha(clusters,points_per_cluster,max_alpha=max_alpha)])
     ##
-    mle_vals = np.array(mle_vals)
+    mle_vals = numpy.array(mle_vals)
     pylab.figure()
     pylab.loglog(mle_vals[:,0],mle_vals[:,1],color='white') ##just create the axes
     pylab.xlabel("clusters")
