7652fdeacb8006de0bb7e7dab52ebb5fd506584c
dlovell <dlovell@gmail.com>
2012-09-19 09:23:19 -0400

    make pyx functions compilation not depend on path info move variables into classes for organizational purposes

--- examples/DPMB/settings.py
+++ examples/DPMB/settings.py
@@ -1,28 +1,35 @@
 #!python
 import os
 
+class env():
+    is_windows = None
+    is_aws = False
+    if os.sys.platform in {'cygwin':None,"win32":None}:
+        is_windows = True
+    else:
+        is_windows = False
 
-# resolve directories to use
-root_dir = None
-is_windows = None
-is_aws = False
-if os.sys.platform in {'cygwin':None,"win32":None}:
-    root_dir = "c:" + os.path.sep
-    is_windows = True
-else:
-    root_dir = os.path.join(os.path.sep,"usr","local")
-    is_windows = False
-base_dir = os.path.join(root_dir,"Cloudless","examples","DPMB")
-#
-if not os.path.isdir(base_dir): # assume AWS
-    root_dir = os.path.join(
-        os.path.sep,"usr","local","lib","python2.7","dist-packages")
+class path():
+    root_dir = None
+    if env.is_windows:
+        root_dir = "c:" + os.path.sep
+    else:
+        root_dir = os.path.join(os.path.sep,"usr","local")
+    #
     base_dir = os.path.join(root_dir,"Cloudless","examples","DPMB")
-    is_aws = True
-#
-data_dir = os.path.join(base_dir,"Data")
-output_dir = os.path.join(base_dir,"Output")
+    if not os.path.isdir(base_dir): # assume AWS
+        root_dir = os.path.join(
+            os.path.sep,"usr","local","lib","python2.7","dist-packages")
+        base_dir = os.path.join(root_dir,"Cloudless","examples","DPMB")
+        env.is_aws = True
+    #
+    # data_dir = os.path.join(base_dir,"Data")
+    data_dir = '/tmp/'
+    output_dir = os.path.join(base_dir,"Output")
 
+data_dir = path.data_dir # FIXME: remove this when dependent code finally changed
+
+# ensure data_dir exists
 try:
     os.makedirs(data_dir)
 except OSError, e:
@@ -32,20 +39,19 @@ except OSError, e:
 try:
     import Cloudless.examples.DPMB.pyx_functions
 except ImportError:
-    os.system(
-        " ".join([
-        'bash',
-        os.path.join(base_dir,"compile_pyx_functions.sh"),
-        base_dir,
-        ]))
-#
+    pyx_filename = 'compile_pyx_functions.sh'
+    system_str = ' '.join(['bash', pyx_filename, '.'])
+    os.system(system_str)
+
+# ensure pandas, mrjob on system
+# FIXME: move this to some dependencies file like pip's requirements
 try:
     import pandas
 except ImportError:
-    os.system(
-        'easy_install http://pypi.python.org/packages/source/p/' \
-            'pandas/pandas-0.7.0rc1.tar.gz'
-        )
+    pandas_uri = 'http://pypi.python.org/packages/source/p/pandas/' + \
+        'pandas-0.7.0rc1.tar.gz'
+    system_str = ' '.join(['easy_install', pandas_uri])
+    os.system(system_str)
 #
 try:
     import mrjob
@@ -53,8 +59,9 @@ except ImportError:
     os.system('easy_install mrjob')
 
 # gdocs
-auth_file = os.path.expanduser("~/mh_gdocs_auth")
-gdocs_folder_default = "MH"
+class gdocs():
+    auth_file = os.path.expanduser("~/mh_gdocs_auth")
+    gdocs_folder_default = "MH"
 
 # cifar
 cifar_10_problem_file = "cifar_10_problem.pkl.gz"
@@ -67,14 +74,15 @@ clustering_dir = "Current_clusterings"
 # tiny images
 # tiny_image_dir only necessary for pulling actual image data
 tiny_image_dir = '/mnt/TinyImages' \
-                 if is_aws else '/media/VonNeumann/TinyImages'
+                 if env.is_aws else '/media/VonNeumann/TinyImages'
 tiny_image_problem_file = 'tiny_image_problem_nImages_' \
     '1000000_nPcaTrain_400000.pkl.gz'
 
 
-# s3
-home_dir = os.path.expanduser("~/")
-bucket_str = "dpmb.mitprobabilisticcomputingproj"
-bucket_dir = ""
-ec2_auth_key = os.path.expanduser("~/awsKeyPair.pem")
-ec2_credentials_file = os.path.expanduser("~/.boto")
+class s3():
+    bucket_str = 'dpmb.mitprobabilisticcomputingproj'
+    # bucket_str = 'mitpcp_test_bucket'
+    bucket_dir = ''
+    #
+    # ec2_auth_key = os.path.expanduser('~/awsKeyPair.pem')
+    # ec2_credentials_file = os.path.expanduser('~/.boto')
