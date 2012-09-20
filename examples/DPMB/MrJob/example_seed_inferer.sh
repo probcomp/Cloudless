f865d28ba86d48ad9bdf9ae036ac091e82ad9689
dlovell <dlovell@gmail.com>
2012-09-20 18:51:27 -0400

    uncomment multi-node runs

--- examples/DPMB/MrJob/example_seed_inferer.sh
+++ examples/DPMB/MrJob/example_seed_inferer.sh
@@ -7,10 +7,10 @@ consolidate_script="${mrjob_dir}/consolidate_summaries.py"
 cd $data_dir
 printf "1\n" > seed_list.txt
 printf "using seed_list.txt:\n`cat seed_list.txt`\n"
-# python "${inferer_script}" --jobconf mapred.map.tasks=4 --num-iters 8 --num-nodes 4 \
-#     < seed_list.txt >/dev/null
-# python "${inferer_script}" --jobconf mapred.map.tasks=2 --num-iters 4 --num-nodes 2 \
-#     < seed_list.txt >/dev/null
+python "${inferer_script}" --jobconf mapred.map.tasks=4 --num-iters 8 --num-nodes 4 \
+    < seed_list.txt >/dev/null
+python "${inferer_script}" --jobconf mapred.map.tasks=2 --num-iters 4 --num-nodes 2 \
+    < seed_list.txt >/dev/null
 python "${inferer_script}" --jobconf mapred.map.tasks=1 --num-iters 2 --num-nodes 1 \
     < seed_list.txt >/dev/null
 
