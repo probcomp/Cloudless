a44ab61983f58d5b2067546572485c3b68f9ad58
dlovell <dlovell@gmail.com>
2012-09-23 20:57:53 -0400

    lower beta_d, save generated problem image

--- examples/DPMB/MrJob/programatic_mrjob.py
+++ examples/DPMB/MrJob/programatic_mrjob.py
@@ -2,6 +2,9 @@
 import os
 from collections import namedtuple
 #
+import matplotlib
+matplotlib.use('Agg')
+#
 import Cloudless.examples.DPMB.settings as S
 reload(S)
 import Cloudless.examples.DPMB.MrJob.seed_inferer as si
@@ -17,16 +20,18 @@ gen_seed = 0
 num_rows = 1024
 num_cols = 256
 num_clusters = 4
-beta_d = 10.
+beta_d = 2.
 seed_filename = 'seed_list.txt'
+image_save_str = os.path.join(S.data_dir, 'mrjob_problem_gen_state')
 #
 problem, problem_filename = csd.pkl_mrjob_problem(
-    gen_seed, num_rows, num_cols, num_clusters, beta_d)
+    gen_seed, num_rows, num_cols, num_clusters, beta_d,
+    image_save_str=image_save_str)
 seed_full_filename = os.path.join(S.data_dir, seed_filename)
 os.system('printf "0\n" > ' + seed_full_filename)
 
 # inference settings
-num_iters = 3
+num_iters = 7
 num_nodes_list = [1, 2, 4]
 gibbs_init_filename = 'gibbs_init.pkl.gz'
 
@@ -50,6 +55,7 @@ with mr_job.make_runner() as runner:
 
 # now run for each num_nodes
 for num_nodes in num_nodes_list:
+    print 'starting num_nodes = ' + str(num_nodes)
     infer_args = ['--resume-file', gibbs_init_filename]
     infer_args.extend(create_args(num_iters, num_nodes))
     mr_job = si.MRSeedInferer(args=infer_args)
