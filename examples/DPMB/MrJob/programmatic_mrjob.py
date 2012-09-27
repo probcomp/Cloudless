1093e1b996e134a10311b9fdc963505bbf824e60
dlovell <dlovell@gmail.com>
2012-09-26 22:32:01 -0400

    extract reduced summaries, pickle to data dir save run parameters to data dir

--- examples/DPMB/MrJob/programmatic_mrjob.py
+++ examples/DPMB/MrJob/programmatic_mrjob.py
@@ -15,6 +15,8 @@ import Cloudless.examples.DPMB.Tests.create_synthetic_data as csd
 reload(csd)
 import Cloudless.examples.DPMB.MrJob.consolidate_summaries as cs
 reload(cs)
+import Cloudless.examples.DPMB.remote_functions as rf
+reload(rf)
 
 
 # parse some arguments
@@ -50,6 +52,7 @@ image_save_str = 'mrjob_problem_gen_state'
 gibbs_init_filename = 'gibbs_init.pkl.gz'
 data_dir_prefix = 'programmatic_mrjob_'
 parameters_filename = 'run_parameters.txt'
+reduced_summaries_name = 'reduced_summaries.pkl.gz'
 
 # determine data dir
 get_hexdigest = lambda variable: \
@@ -98,16 +101,16 @@ for num_nodes in num_nodes_list:
     with mr_job.make_runner() as runner:
         runner.run()
 
+# summarize the data
 summaries_dict, numnodes1_seed1 = cs.read_summaries([data_dir])
 cs.plot_summaries(summaries_dict, plot_dir=data_dir)
+reduced_summaries_dict = cs.extract_reduced_summaries(
+    summaries_dict, cs.reduced_summary_extract_func_tuples)
 
-# create dir for results
-this_file = __file__
-data_files = os.path.join(data_dir, '*{png,txt,pkl.gz}')
-#
-system_str = ' '.join(['cp', this_file, data_dir])
-os.system(system_str)
-
-# system_str = ' '.join(['mv', data_files, data_dir])
-# system_str = ' '.join(['echo', system_str, '| bash'])
-# os.system(system_str)
+# save some consolidated info
+rf.pickle(reduced_summaries_dict, reduced_summaries_name, dir=data_dir)
+parameters_full_filename = os.path.join(data_dir, parameters_filename)
+with open(parameters_full_filename, 'w') as fh:
+    for key, value in vars(args).iteritems():
+        line = str(key) + ' = ' + str(value) + '\n'
+        fh.write(line)
