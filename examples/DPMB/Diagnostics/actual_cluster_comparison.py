16d3ea93ebcb55042dca036b969d102a6cd19a19
dlovell <dlovell@gmail.com>
2012-09-27 21:47:14 -0400

    dont plot high beta add more jitter put legend outsize plot

--- examples/DPMB/Diagnostics/actual_cluster_comparison.py
+++ examples/DPMB/Diagnostics/actual_cluster_comparison.py
@@ -12,7 +12,8 @@ reload(rf)
 
 
 # some settings
-base_dir = '/usr/local/Cloudless/examples/DPMB/Data'
+# base_dir = '/usr/local/Cloudless/examples/DPMB/Data'
+base_dir = '/tmp'
 data_dir_prefix = 'programmatic_mrjob_'
 parameters_filename = 'run_parameters.txt'
 reduced_summaries_name = 'reduced_summaries.pkl.gz'
@@ -43,6 +44,8 @@ for programmatic_dir in programmatic_dirs:
     if not os.path.isfile(parameters_full_filename): continue
     parameters = dict()
     exec open(parameters_full_filename) in parameters
+    if parameters['beta_d'] > 1.0: continue
+    #
     reduced_summaries = rf.unpickle(reduced_summaries_name, dir=data_dir)
     #
     true_clusters = parameters['num_clusters']
@@ -60,24 +63,41 @@ for key in results_by_numnodes:
 
 color_lookup = dict(zip([1, 2, 4], ['red', 'blue', 'green']))
 marker_lookup = dict(zip([1, 2, 4], ['+', 'x', 'v']))
-jitter_lookup = dict(zip([1, 2, 4], [.98, 1., 1.02]))
+h_jitter_add_lookup = dict(zip([1, 2, 4], [.9, 1., 1.1]))
 ax = None
 random_state = numpy.random.RandomState(0)
-v_jitter_delta = .02
+v_jitter_delta = .1
 v_low = 1 - v_jitter_delta
 v_high = 1 + v_jitter_delta
 for numnodes, series in series_dict.iteritems():
     color = color_lookup[numnodes]
     marker = marker_lookup[numnodes]
-    h_jitter = jitter_lookup[numnodes]
+    h_jitter_add = h_jitter_add_lookup[numnodes]
+    h_jitter_mult = random_state.uniform(
+        low=v_low, high=v_high, size=len(series))
     v_jitter = random_state.uniform(low=v_low, high=v_high, size=len(series))
-    xs = series.index * h_jitter
+    xs = series.index * h_jitter_add * h_jitter_mult
     ys = series.values * v_jitter
-    ax = log_log_plot(xs, ys, ax=ax, color=color, marker=marker)
+    label = 'numnodes=' + str(numnodes)
+    ax = log_log_plot(xs, ys, ax=ax, color=color, marker=marker, label=label)
 
-legend_list = ['numnodes=' + str(key) for key in series_dict.keys()]
-pylab.legend(legend_list)
+uniq_true_num_clusters = numpy.unique([el for el in series.index])
+for true_num_clusters in uniq_true_num_clusters:
+    ax.axvline(true_num_clusters, color='red')
+
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+pylab.plot(xlim, ylim, color='red')
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
+
+handles, labels = ax.get_legend_handles_labels()
+lgd = ax.legend(handles, labels, loc='upper center', ncol=3,
+                bbox_to_anchor=(0.5,-0.1))
 pylab.xlabel('true num clusters')
 pylab.ylabel('last sample num clusters')
-pylab.title('Final num clusters comparison for comparable # iters')
-pylab.savefig('true_vs_sampled_num_clusters')
+pylab.title('Final num clusters comparison for comparable problems, #iters\n'
+            'Vertical and Horizontal jitter added\n'
+            'true num clusters denoted by proximate red line')
+pylab.savefig('true_vs_sampled_num_clusters',
+              bbox_extra_artists=(lgd,), bbox_inches='tight')
