815ad9aefd4ef7d713b380e8dc7824489966f59f
dlovell <dlovell@gmail.com>
2012-09-28 11:19:45 -0400

    move visualize_mle_alpha to helper_functions.py

--- examples/DPMB/plot_utils.py
+++ examples/DPMB/plot_utils.py
@@ -51,23 +51,3 @@ def multiplot(data, plot_tuples, title='', xlabel='', save_str=None,
     if save_str is not None:
         savefig_legend_outside(save_str, last_axes)
     return fh
-    
-def visualize_mle_alpha(cluster_list=None,points_per_cluster_list=None,max_alpha=None):
-    import pylab
-    cluster_list = cluster_list if cluster_list is not None else 10**numpy.arange(0,4,.5)
-    points_per_cluster_list = points_per_cluster_list if points_per_cluster_list is not None else 10**numpy.arange(0,4,.5)
-    max_alpha = max_alpha if max_alpha is not None else int(1E4)
-    ##
-    mle_vals = []
-    for clusters in cluster_list:
-        for points_per_cluster in points_per_cluster_list:
-            mle_vals.append([clusters,points_per_cluster,dm.mle_alpha(clusters,points_per_cluster,max_alpha=max_alpha)])
-    ##
-    mle_vals = numpy.array(mle_vals)
-    pylab.figure()
-    pylab.loglog(mle_vals[:,0],mle_vals[:,1],color='white') ##just create the axes
-    pylab.xlabel("clusters")
-    pylab.ylabel("points_per_cluster")
-    pylab.title("MLE alpha for a given data configuration\nmax alpha: "+str(max_alpha-1))
-    for clusters,points_per_cluster,mle in mle_vals:
-        pylab.text(clusters,points_per_cluster,str(int(mle)),color='red')
