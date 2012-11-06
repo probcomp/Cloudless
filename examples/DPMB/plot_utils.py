8932a88c23213e64a72c68dc5f8fe5e26f8b681f
dlovell <dlovell@gmail.com>
2012-11-06 11:56:37 -0500

    add helper function to only add legend lines for unique labels

--- examples/DPMB/plot_utils.py
+++ examples/DPMB/plot_utils.py
@@ -15,12 +15,23 @@ def get_gridspec(height_ratios_or_int):
         height_ratios = numpy.repeat(1./height_ratios, height_ratios)
     return gridspec.GridSpec(len(height_ratios), 1, height_ratios=height_ratios)
 
+def get_unique_handles_labels(handles, labels):
+    handles_lookup = dict()
+    for handle, label in zip(handles, labels):
+        if label not in handles_lookup:
+            handles_lookup[label] = handle
+    unique_labels = handles_lookup.keys()
+    unique_handles = handles_lookup.values()
+    return unique_handles, unique_labels
+
 def legend_outside(ax=None, bbox_to_anchor=(0.5, -.25), loc='upper center',
-                   ncol=None):
+                   ncol=None, unique=False):
     # labels must be set in original plot call: plot(..., label=label)
     if ax is None:
         ax = pylab.gca()
     handles, labels = ax.get_legend_handles_labels()
+    if unique:
+        handles, labels = get_unique_handles_labels(handles, labels)
     if ncol is None:
         ncol = len(labels)
     lgd = ax.legend(handles, labels, loc=loc, ncol=ncol,
