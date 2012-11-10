2a5abdbb66cae7bebcfe2015b6140343cd4f89c9
dlovell <dlovell@gmail.com>
2012-11-10 17:05:44 -0500

    take an argument for number of core instances

--- examples/DPMB/MrJob/create_job_flow.sh
+++ examples/DPMB/MrJob/create_job_flow.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+# usage: bash create_job_flow.sh num_ec2_core_instances
 
-
-ec2_instance_type=c1.medium
+if [ -z $1 ] ; then
+    echo "usage: bash create_job_flow.sh num_ec2_core_instances";
+    exit;
+fi
 # settings
-num_ec2_instances=5
-num_iters=10
-num_iters_per_step=4
-#
-num_workers=$(( $num_ec2_instances - 1 ))
-num_nodes=$(( $num_workers * 2 ))
-num_map_tasks=$(( $num_nodes + 1 ))
+ec2_instance_type=c1.medium
+num_ec2_instances=$(( $1 + 1 ))
 
 # spin up a job flow
 cd /home/dlovell/mrjob/ # this is where I pulled mrjob.git down to
@@ -20,18 +18,6 @@ python -m /home/dlovell/mrjob/mrjob/tools/emr/create_job_flow \
 emr_job_flow_id=$(tail -n 1 create_job_flow.out)
 echo $emr_job_flow_id
 # --ec2-instance-type $ec2_instance_type \
-
-# cd $DPMB/MrJob
-# run_dir=programmatic_mrjob_437b056c5a
-# seed_list=seed_list.txt
-# python $DPMB/MrJob/seed_inferer.py -r emr --emr-job-flow-id=$emr_job_flow_id \
-#     --pool-wait-minutes 30 \
-#     --jobconf mapred.map.tasks=$num_map_tasks --num-nodes $num_nodes \
-#     --resume-file gibbs_init.pkl.gz --problem-file problem.pkl.gz \
-#     --run_dir $run_dir \
-#     --num-iters $num_iters --num-iters-per-step $num_iters_per_step \
-#     --push_to_s3 \
-#     $seed_list > out
 
 # this is supposed to run a command on all nodes but doesn't 
 # python -m mrjob.tools.emr.mrboss $emr_job_flow_id -v 'cd /usr/local/lib/python2.7/site-packages/Cloudless/ && git pull'
