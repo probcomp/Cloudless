dc1e089859f20256569aa62926aac6de69b04ded
dlovell <dlovell@gmail.com>
2012-11-11 19:45:51 -0500

    reorganize

--- examples/DPMB/MrJob/create_job_flow.sh
+++ examples/DPMB/MrJob/create_job_flow.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 # usage: bash create_job_flow.sh num_ec2_core_instances
 
-ec2-instance-type $ec2_instance_type 
-ec2_instance_type=c1.medium
-# parse args
+
 if [ -z $1 ] ; then
     echo "usage: bash create_job_flow.sh num_ec2_core_instances";
     exit;
 fi
-if [ ! -z $2 ] ; then
-    ec2_instance_type=$2
-fi
 
 # settings
 num_ec2_instances=$(( $1 + 1 ))
+ec2_instance_type=c1.xlarge
+if [ ! -z $2 ] ; then
+    ec2_instance_type=$2
+fi
 
 # spin up a job flow
 cd /home/dlovell/mrjob/ # this is where I pulled mrjob.git down to
