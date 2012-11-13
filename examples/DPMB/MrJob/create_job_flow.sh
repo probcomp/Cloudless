095b11960bacccf52e131a8c1cde5a9491fa7823
dlovell <dlovell@gmail.com>
2012-11-13 09:47:54 -0500

    intelligently handle num_ec2_instances==1

--- examples/DPMB/MrJob/create_job_flow.sh
+++ examples/DPMB/MrJob/create_job_flow.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # usage: bash create_job_flow.sh num_ec2_core_instances
-
-
 if [ -z $1 ] ; then
     echo "usage: bash create_job_flow.sh num_ec2_core_instances";
     exit;
 fi
 
 # settings
-num_ec2_instances=$(( $1 + 1 ))
+num_ec2_core_instances=$1
+if [ $num_ec2_core_instances == 1 ] ; then
+    num_ec2_instances=1
+else
+    num_ec2_instances=$(( $1 + 1 ))
+fi
+#
 ec2_instance_type=c1.xlarge
 if [ ! -z $2 ] ; then
     ec2_instance_type=$2
@@ -20,7 +24,9 @@ python -m /home/dlovell/mrjob/mrjob/tools/emr/create_job_flow \
     --num-ec2-instances $num_ec2_instances \
     --ec2-instance-type $ec2_instance_type \
     --bootstrap-action $DPMB/bootstrap.sh \
-    --ec2-master-instance-type c1.xlarge
-    > create_job_flow.out
+    --ec2-master-instance-type c1.xlarge >> create_job_flow.out
+    
 emr_job_flow_id=$(tail -n 1 create_job_flow.out)
+echo $ec2_instance_type
+echo $num_ec2_instances
 echo $emr_job_flow_id
