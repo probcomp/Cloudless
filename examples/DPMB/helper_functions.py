643c95b96f210e57a05a49672e18fd725f9e7686
dlovell <dlovell@gmail.com>
2012-11-14 11:43:19 -0500

    mle_alpha(): return alpha pdf

--- examples/DPMB/helper_functions.py
+++ examples/DPMB/helper_functions.py
@@ -186,10 +186,11 @@ def calculate_node_conditional(pstate,cluster):
     return conditionals
 
 def mle_alpha(clusters,points_per_cluster,max_alpha=100):
-    mle = 1+np.argmax([ss.gammaln(alpha) + clusters*np.log(alpha) 
+    alpha_ps = [ss.gammaln(alpha) + clusters*np.log(alpha) 
                        - ss.gammaln(clusters*points_per_cluster+alpha) 
-                       for alpha in range(1,max_alpha)])
-    return mle
+                       for alpha in range(1,max_alpha)]
+    mle = 1+np.argmax(alpha_ps)
+    return mle, alpha_ps
 
 def mhSample(initVal,nSamples,lnPdf,sampler,random_state):
     samples = [initVal]
