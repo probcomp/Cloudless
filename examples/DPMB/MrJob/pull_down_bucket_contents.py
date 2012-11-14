5817d1c2f9b0b48ec5fd79c20d054a421c6d5825
dlovell <dlovell@gmail.com>
2012-11-14 12:13:19 -0500

    add filename_{prefix,suffix} arguments for file filtering

--- examples/DPMB/MrJob/pull_down_bucket_contents.py
+++ examples/DPMB/MrJob/pull_down_bucket_contents.py
@@ -11,8 +11,12 @@ local_dir_base = '/tmp'
 # parse args
 parser = argparse.ArgumentParser()
 parser.add_argument('--bucket_dir_suffix', default='', type=str)
+parser.add_argument('--filename_prefix', default='', type=str)
+parser.add_argument('--filename_suffix', default='', type=str)
 args = parser.parse_args()
 bucket_dir_suffix = args.bucket_dir_suffix
+filename_prefix = args.filename_prefix
+filename_suffix = args.filename_suffix
 
 # set up path variables
 bucket_dir = os.path.join(bucket_dir_base, bucket_dir_suffix)
@@ -26,4 +30,6 @@ bucket_els = [el for el in bucket_dir_list if el.name != bucket_dir]
 # now pull
 for bucket_el in bucket_els:
     filename = os.path.split(bucket_el.name)[-1]
+    if not filename.startswith(filename_prefix): continue
+    if not filename.endswith(filename_suffix): continue
     s3.verify_file(filename)
