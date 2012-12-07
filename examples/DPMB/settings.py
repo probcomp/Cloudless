adf6df0a59da5c3124b726fc23635c365c3b31ca
dlovell <dlovell@gmail.com>
2012-12-07 01:16:51 -0500

    change run_dir_prefix to more easily identify new problems generated

--- examples/DPMB/settings.py
+++ examples/DPMB/settings.py
@@ -32,7 +32,7 @@ class files():
     problem_filename = 'problem.pkl.gz'
     gen_state_image_save_str = 'mrjob_problem_gen_state'
     gibbs_init_filename = 'gibbs_init.pkl.gz'
-    run_dir_prefix = 'programmatic_mrjob_'
+    run_dir_prefix = 'new_programmatic_mrjob_'
     parameters_filename = 'run_parameters.txt'
     reduced_summaries_name = 'reduced_summaries.pkl.gz'
     
