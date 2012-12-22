411563a5a547814fb8dc04649c8bff4998c8c0ff
dlovell <dlovell@gmail.com>
2012-12-22 01:34:15 -0500

    change format of print

--- examples/DPMB/MrJob/read_summary.py
+++ examples/DPMB/MrJob/read_summary.py
@@ -15,7 +15,10 @@ clusters_per_node = map(len, summary['lolo_x_indices'])
 datapoints_per_node = map(lambda x: sum(map(len, x)), summary['lolo_x_indices'])
 
 argsort_indices = numpy.argsort(clusters_per_node)[::-1]
-print 'clusters_per_node'
-print numpy.array(clusters_per_node)[argsort_indices]
-print 'datapoints_per_node'
-print numpy.array(datapoints_per_node)[argsort_indices]
+clusters_per_node = numpy.array(clusters_per_node)[argsort_indices]
+datapoints_per_node = numpy.array(datapoints_per_node)[argsort_indices]
+zipped = zip(clusters_per_node, datapoints_per_node)
+zipped = filter(lambda x: x[0]!=0, zipped)
+
+print '(clusters_per_node, datapoints_per_node) x', len(zipped)
+print zipped
