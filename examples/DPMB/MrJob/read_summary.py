7c1c83688808a4f27da6f96140af83381529d245
dlovell <dlovell@gmail.com>
2012-12-22 00:34:09 -0500

    sort {clusters,datapoints}_per_node

--- examples/DPMB/MrJob/read_summary.py
+++ examples/DPMB/MrJob/read_summary.py
@@ -1,5 +1,7 @@
 import argparse
 #
+import numpy
+#
 import Cloudless.examples.DPMB.remote_functions as rf
 
 
@@ -9,5 +11,11 @@ args = parser.parse_args()
 summary_filename = args.summary_filename
 
 summary = rf.unpickle(summary_filename)
-print map(len, summary['lolo_x_indices'])
-print map(lambda x: sum(map(len, x)), summary['lolo_x_indices'])
+clusters_per_node = map(len, summary['lolo_x_indices'])
+datapoints_per_node = map(lambda x: sum(map(len, x)), summary['lolo_x_indices'])
+
+argsort_indices = numpy.argsort(clusters_per_node)[::-1]
+print 'clusters_per_node'
+print numpy.array(clusters_per_node)[argsort_indices]
+print 'datapoints_per_node'
+print numpy.array(datapoints_per_node)[argsort_indices]
