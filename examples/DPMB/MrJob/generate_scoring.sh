c49c46ce4665f7cbc31c29a87d6452b3dc26ea14
dlovell <dlovell@gmail.com>
2012-12-23 12:45:37 -0500

    allow passing of configuration arguments

--- examples/DPMB/MrJob/generate_scoring.sh
+++ examples/DPMB/MrJob/generate_scoring.sh
@@ -1,24 +1,33 @@
-run_dir_list=($(ls /mnt/ | grep ^new_prog))
+#!bash
+# USAGE: generate_scoring.sh SLEEP_DUR NUM_WORKERS BASE_DIR
+if [ -z $3 ] ; then
+    echo "USAGE: generate_scoring.sh SLEEP_DUR NUM_WORKERS BASE_DIR"
+    exit
+fi
 
-sleep_dur=60
-num_workers=3
 
-function process_run_dir() {
+sleep_dur=$1
+num_workers=$2
+base_dir=$3
+run_dir_list=($(ls $base_dir | grep ^new_prog))
+
+
+function process_single_run_dir() {
     sleep_dur=$1
     num_workers=$2
     run_dir=$3
-    echo "`date` :: starting :: process_run_dir $sleep_dur $num_workers $run_dir"
+    echo "`date` :: starting :: process_single_run_dir $sleep_dur $num_workers $run_dir"
     python generate_scoring.py $run_dir --is_controller --num_workers 0 --do_create_queue
     sleep $sleep_dur
     for worker_idx in $(seq $num_workers); do
 	python generate_scoring.py $run_dir &
     done
     wait
-    echo "`date` :: finished :: process_run_dir $sleep_dur $num_workers $run_dir"
+    echo "`date` :: finished :: process_single_run_dir $sleep_dur $num_workers $run_dir"
 }
 
 while true; do
     for run_dir in ${run_dir_list[*]}; do
-	process_run_dir $sleep_dur $num_workers $run_dir
+	process_single_run_dir $sleep_dur $num_workers $run_dir
     done
 done
