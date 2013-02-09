1cf6c347dc8e20d3b4cdc632365761451ba65fff
dan lovell <dlovell@gmail.com>
2013-02-08 19:02:19 -0500

    dont gibbs init

--- examples/DPMB/MrJob/create_hadoop_problem.py
+++ examples/DPMB/MrJob/create_hadoop_problem.py
@@ -62,24 +62,23 @@ create_args = lambda num_iters, num_nodes: [
     '--num-nodes', str(num_nodes),
     '--problem-file', problem_filename,
     '--run_dir', run_dir,
-    seed_full_filename,
+    seed_filename,
     ]
 
 parameters = pop_args(vars(args), ignore_params)
 # determine some path variables
 hex_digest = dict_to_hexdigest(parameters)
 run_dir = run_dir_prefix + hex_digest
-run_full_dir = os.path.join(data_dir, run_dir)
-seed_full_filename = os.path.join(run_full_dir, 'fake_seed_for_gen.txt')
-init_full_filename = os.path.join(run_full_dir, init_filename)
-parameters_full_filename = os.path.join(run_full_dir, parameters_filename)
+seed_filename = os.path.join(run_dir, 'fake_seed_for_gen.txt')
+init_full_filename = os.path.join(run_dir, init_filename)
+parameters_full_filename = os.path.join(run_dir, parameters_filename)
 #
 summary_bucket_dir = S.s3.summary_bucket_dir
 run_bucket_dir = os.path.join(summary_bucket_dir, run_dir)    
 #
-print run_full_dir
+print run_dir
 try:
-    os.makedirs(run_full_dir)
+    os.makedirs(run_dir)
 except OSError, ose:
     print ose
 
@@ -87,9 +86,9 @@ except OSError, ose:
 problem, problem_filename = csd.pkl_mrjob_problem(
     gen_seed, num_rows, num_cols, num_clusters, beta_d,
     problem_filename=problem_filename,
-    image_save_str=image_save_str, dir='.')
+    image_save_str=image_save_str, dir=run_dir)
 # save the initial parameters
-with open(parameters_filename, 'w') as fh:
+with open(parameters_full_filename, 'w') as fh:
     for key, value in parameters.iteritems():
         line = str(key) + ' = ' + str(value) + '\n'
         fh.write(line)
@@ -99,15 +98,15 @@ for filename in [problem_filename, h5_filename, parameters_filename]:
 
 # do I need to push to s3 so that crp init can read?
 
-# gibbs init to be used by all subsequent inference
-init_args = ['--init-file', init_filename, '--file', problem_filename, '--file', h5_filename]
-init_num_iters = 0
-init_num_nodes = 5 # FIXME: this is what you vary to make crp init quicker
-init_args.extend(create_args(init_num_iters, init_num_nodes))
-os.system('printf "' + str(gen_seed) + '\n" > ' + seed_full_filename)
-mr_job = si.MRSeedInferer(args=init_args)
-with mr_job.make_runner() as runner:
-    runner.run()
+# # gibbs init to be used by all subsequent inference
+# init_args = ['--init-file', init_filename, '--file', problem_filename, '--file', h5_filename]
+# init_num_iters = 0
+# init_num_nodes = 5 # FIXME: this is what you vary to make crp init quicker
+# init_args.extend(create_args(init_num_iters, init_num_nodes))
+# os.system('printf "' + str(gen_seed) + '\n" > ' + seed_filename)
+# mr_job = si.MRSeedInferer(args=init_args)
+# with mr_job.make_runner() as runner:
+#     runner.run()
 
 # for hadoop
 source_full_dir = os.path.join('/user/sgeadmin/', run_dir)
@@ -116,7 +115,7 @@ os.system(hadoop_fs_cmd)
 run_bucket_dir = os.path.join(summary_bucket_dir, run_dir)
 s3 = s3h.S3_helper(bucket_dir=run_bucket_dir, local_dir=run_dir)
 for filename in \
-        [problem_filename, h5_filename, parameters_filename, init_filename]:
+        [problem_filename, h5_filename, parameters_filename]: # , init_filename]:
     s3.put_s3(filename)
 
 print run_dir
