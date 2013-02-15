f369d4c930a29ccc59887d39aab02e2f4b18da0d
dan lovell <dlovell@gmail.com>
2013-02-14 20:05:26 -0500

    sort legend by worker count

--- examples/DPMB/plot_utils.py
+++ examples/DPMB/plot_utils.py
@@ -25,17 +25,19 @@ def get_unique_handles_labels(handles, labels):
     return unique_handles, unique_labels
 
 def legend_outside(ax=None, bbox_to_anchor=(0.5, -.25), loc='upper center',
-                   ncol=None, unique=True, sort=False):
+                   ncol=None, unique=True, cmp_func=cmp):
     # labels must be set in original plot call: plot(..., label=label)
     if ax is None:
         ax = pylab.gca()
     handles, labels = ax.get_legend_handles_labels()
     if unique:
         handles, labels = get_unique_handles_labels(handles, labels)
-    if sort:
-        sorted_indices = pylab.argsort(labels)[::-1]
-        handles = pylab.array(handles)[sorted_indices]
-        labels = pylab.array(labels)[sorted_indices]
+    zipped = zip(handles, labels)
+    cmp_func_mod = lambda x, y: cmp_func(x[-1], y[-1])
+    sorted_zipped = sorted(zipped, cmp=cmp_func_mod)[::-1]
+    handles, labels = zip(*sorted_zipped)
+    handles = pylab.array(handles)
+    labels = pylab.array(labels)
     if ncol is None:
         ncol = min(len(labels), 4)
     lgd = ax.legend(handles, labels, loc=loc, ncol=ncol,
@@ -52,8 +54,18 @@ def savefig_legend_outside(namestr, ax=None, bbox_inches='tight'):
         print e
         pylab.savefig(namestr)
 
+def try_intify(to_intify):
+    intified = to_intify
+    try:
+        intified = int(to_intify)
+    except Exception, e:
+        pass
+    return intified
+get_after_equals_as_int = lambda x: try_intify(x.split('=')[-1])
+cmp_after_equals_as_int = lambda x, y: cmp(get_after_equals_as_int(x),
+                                           get_after_equals_as_int(y))
 def multiplot(data, plot_tuples, title='', xlabel='', save_str=None,
-              subplots_hspace=.25, sort=True):
+              subplots_hspace=.25, cmp_func=cmp_after_equals_as_int):
     num_tuples = len(plot_tuples)
     gs = get_gridspec(num_tuples)
     fh = pylab.figure()
@@ -65,7 +77,7 @@ def multiplot(data, plot_tuples, title='', xlabel='', save_str=None,
         pylab.ylabel(ylabel)
         last_axes.set_xlim((0, last_axes.get_xlim()[-1]))
     pylab.subplots_adjust(hspace=subplots_hspace)
-    legend_outside(last_axes, sort=sort)
+    legend_outside(last_axes, cmp_func=cmp_func)
     first_axes = fh.get_axes()[0]
     first_axes.set_title(title)
     last_axes.set_xlabel(xlabel)
