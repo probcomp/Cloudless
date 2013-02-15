dcfadba8369e0f0fb3d84fadef40df7deee454da
dan lovell <dlovell@gmail.com>
2013-02-15 13:59:19 -0500

    add random image generation

--- examples/DPMB/TinyImages/render_from_file.py
+++ examples/DPMB/TinyImages/render_from_file.py
@@ -1,11 +1,11 @@
 import os
+import argparse
 #
 import numpy
 import pylab
 #
 import Cloudless.examples.DPMB.TinyImages.render_images as ri
 import Cloudless.examples.DPMB.remote_functions as rf
-
 
 data_dir = '/mnt/new_programmatic_mrjob_tiny_images_1MM/'
 problem_filename = 'problem.pkl.gz'
@@ -15,7 +15,8 @@ dest_dir = 'RenderedImages'
 dest_full_dir = os.path.join(data_dir, dest_dir)
 ri.makedirs(dest_full_dir)
 
-summary_filename = 'summary_numnodes128_seed1_he1_iternum3.pkl.gz'
+# summary_filename = 'summary_numnodes128_seed1_he1_iternum3.pkl.gz'
+summary_filename = 'summary_numnodes32_seed15_he1_iternum7.pkl.gz'
 summary_full_filename = os.path.join(data_dir, summary_filename)
 summary = rf.unpickle(summary_full_filename)
 list_of_x_indices = summary['list_of_x_indices']
@@ -46,5 +47,18 @@ for which_cluster in which_clusters:
     montage.save(os.path.join(dest_full_dir, basename + '_images.png'), 'PNG')
     #
     pylab.imshow(problem['xs'][list_of_x_indices[which_cluster]][:how_many_bitvectors])
+    full_figname = os.path.join(dest_full_dir, basename + '_binary.png')
+    pylab.savefig(full_figname)
+
+num_random = max(how_many_bitvectors, how_many_images)
+random_indices_list = numpy.random.randint(len(problem['xs']), size=(3,num_random))
+for random_idx, random_indices in enumerate(random_indices_list):
+    basename = 'random_' + str(random_idx)
+    image_list, image_indices, image_for_rendering_list = \
+        ri.render_problem_image_indices(random_indices, problem)
+    montage = ri.make_contact_sheet(
+        image_for_rendering_list[:how_many_images], (square_len,square_len), dir=dest_full_dir)
+    montage.save(os.path.join(dest_full_dir, basename + '_images.png'), 'PNG')
+    pylab.imshow(problem['xs'][random_indices][:how_many_bitvectors])
     full_figname = os.path.join(dest_full_dir, basename + '_binary.png')
     pylab.savefig(full_figname)
